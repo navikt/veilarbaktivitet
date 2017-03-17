e47322e5d212cfb20652aca6f66aab98e0aa1ce9
Ola <ola.galde@nav.no>
2017-03-17 13:32:00 +0100

    Legger på JaasLogin-config for jetty i WS-modulen.

--- ws/src/test/java/no/nav/fo/StartJetty.java
+++ ws/src/test/java/no/nav/fo/StartJetty.java
@@ -1,6 +1,9 @@
 package no.nav.fo;
 
+import no.nav.modig.core.context.JettySubjectHandler;
+import no.nav.modig.core.context.SubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
+import org.eclipse.jetty.jaas.JAASLoginService;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import static no.nav.fo.veilarbaktivitet.db.DatabaseContext.AKTIVITET_DATA_SOURCE_JDNI_NAME;
@@ -11,10 +14,17 @@ public class StartJetty {
     private static final int PORT = 8486;
 
     public static void main(String[] args) throws Exception {
+        System.setProperty(SubjectHandler.SUBJECTHANDLER_KEY, JettySubjectHandler.class.getName());
+        System.setProperty("java.security.auth.login.config", StartJetty.class.getResource("/login.conf").toExternalForm());
+
+        JAASLoginService jaasLoginService = new JAASLoginService("SAML Realm");
+        jaasLoginService.setLoginModuleName("saml");
+
         Jetty jetty = usingWar()
                 .at("/veilarbaktivitet")
                 .loadProperties("/test.properties")
                 .addDatasource(DatabaseTestContext.buildDataSource(), AKTIVITET_DATA_SOURCE_JDNI_NAME)
+                .withLoginService(jaasLoginService)
                 .port(PORT)
                 .buildJetty();
         jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
