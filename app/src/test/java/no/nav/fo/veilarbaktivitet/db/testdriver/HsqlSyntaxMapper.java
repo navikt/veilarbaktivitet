38836b86b9c8eb674b6b1f68c5d6fba917a9135f
s149594 <alexander.svendsen@nav.no>
2017-03-31 12:17:44 +0200

    Øk størrelsen på beskrivelse og lenke

--- app/src/test/java/no/nav/fo/veilarbaktivitet/db/testdriver/HsqlSyntaxMapper.java
+++ app/src/test/java/no/nav/fo/veilarbaktivitet/db/testdriver/HsqlSyntaxMapper.java
@@ -15,6 +15,11 @@ class HsqlSyntaxMapper {
                 "alter table AKTIVITET modify status varchar(255) not null",
                 "alter table AKTIVITET alter column status SET not null"
         );
+        map(
+                "alter table AKTIVITET modify (beskrivelse varchar(5000), lenke varchar(2000))",
+                "alter table AKTIVITET alter column beskrivelse varchar(5000); alter table AKTIVITET alter column lenke varchar(2000)"
+        );
+
     }
 
     private static void map(String oracleSyntax, String hsqlSyntax) {
---
+++ config/src/main/resources/db/migration/veilarbaktivitetDataSource/V1_10__utvid_aktivitet_beskrivelse.sql
@@ -0,0 +1,1 @@
+alter table AKTIVITET modify (beskrivelse varchar(5000), lenke varchar(2000));
