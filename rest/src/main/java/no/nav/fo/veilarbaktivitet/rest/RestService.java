ba9fdbcc80503ef5b51acda1220d79fffed7a51a
Oscar Eckbo <oscar.eckbo@nav.no>
2017-05-08 15:01:48 +0200

    Legger ved stillingssøketikett også hvis aktivitet er avtalt.

--- rest/src/main/java/no/nav/fo/veilarbaktivitet/rest/RestService.java
+++ rest/src/main/java/no/nav/fo/veilarbaktivitet/rest/RestService.java
@@ -52,6 +52,7 @@ public class RestService implements AktivitetController {
                     val orignalAktivitet = appService.hentAktivitet(Long.parseLong(aktivitet.getId()));
                     if (orignalAktivitet.isAvtalt()) {
                         orignalAktivitet.setTilDato(aktivitet.getTilDato());
+                        orignalAktivitet.setStillingsSoekAktivitetData(aktivitetData.getStillingsSoekAktivitetData());
                         return appService.oppdaterAktivitet(orignalAktivitet);
                     }
                     return appService.oppdaterAktivitet(aktivitetData);
