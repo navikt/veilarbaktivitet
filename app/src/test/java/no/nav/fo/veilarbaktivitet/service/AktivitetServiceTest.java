37645e83d54d41b4390f95ad754ef0d795e830b8
alex <alexander.svendsen@nav.no>
2017-06-09 13:50:09 +0200

    Refaktorer aktivitetService tests

--- app/src/test/java/no/nav/fo/veilarbaktivitet/service/AktivitetServiceTest.java
+++ app/src/test/java/no/nav/fo/veilarbaktivitet/service/AktivitetServiceTest.java
@@ -3,7 +3,10 @@ package no.nav.fo.veilarbaktivitet.service;
 import lombok.val;
 import no.nav.apiapp.feil.VersjonsKonflikt;
 import no.nav.fo.veilarbaktivitet.db.dao.AktivitetDAO;
-import no.nav.fo.veilarbaktivitet.domain.*;
+import no.nav.fo.veilarbaktivitet.domain.AktivitetData;
+import no.nav.fo.veilarbaktivitet.domain.AktivitetStatus;
+import no.nav.fo.veilarbaktivitet.domain.StillingsoekEtikettData;
+import no.nav.fo.veilarbaktivitet.domain.TransaksjonsTypeData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
@@ -16,7 +19,6 @@ import static junit.framework.TestCase.fail;
 import static no.nav.fo.TestData.KJENT_AKTOR_ID;
 import static no.nav.fo.veilarbaktivitet.AktivitetDataTestBuilder.nyAktivitet;
 import static no.nav.fo.veilarbaktivitet.AktivitetDataTestBuilder.nyttStillingssøk;
-import static no.nav.fo.veilarbaktivitet.domain.AktivitetTypeData.EGENAKTIVITET;
 import static no.nav.fo.veilarbaktivitet.domain.AktivitetTypeData.JOBBSOEKING;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -39,193 +41,160 @@ public class AktivitetServiceTest {
 
     @Test
     public void opprettAktivitet() {
-        val aktivitet = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .egenAktivitetData(new EgenAktivitetData())
-                .build();
+        val aktivitet = lagEnNyAktivitet();
 
         when(aktivitetDAO.getNextUniqueAktivitetId()).thenReturn(AKTIVITET_ID);
-
         aktivitetService.opprettAktivitet(KJENT_AKTOR_ID, aktivitet);
 
-        Mockito.verify(aktivitetDAO, times(1)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet = ((AktivitetData) argumentCaptor.getValue());
+        captureInsertAktivitetArgument();
 
-        assertThat(capturedAktiviet.getId(), equalTo(AKTIVITET_ID));
-        assertThat(capturedAktiviet.getFraDato(), equalTo(aktivitet.getFraDato()));
-        assertThat(capturedAktiviet.getTittel(), equalTo(aktivitet.getTittel()));
+        assertThat(getCapturedAktivitet().getId(), equalTo(AKTIVITET_ID));
+        assertThat(getCapturedAktivitet().getFraDato(), equalTo(aktivitet.getFraDato()));
+        assertThat(getCapturedAktivitet().getTittel(), equalTo(aktivitet.getTittel()));
 
-        assertThat(capturedAktiviet.getAktorId(), equalTo(KJENT_AKTOR_ID));
-        assertThat(capturedAktiviet.getTransaksjonsTypeData(), equalTo(TransaksjonsTypeData.OPPRETTET));
-        assertThat(capturedAktiviet.getOpprettetDato(), notNullValue());
+        assertThat(getCapturedAktivitet().getAktorId(), equalTo(KJENT_AKTOR_ID));
+        assertThat(getCapturedAktivitet().getTransaksjonsTypeData(), equalTo(TransaksjonsTypeData.OPPRETTET));
+        assertThat(getCapturedAktivitet().getOpprettetDato(), notNullValue());
     }
 
     @Test
     public void oppdaterStatus() {
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .id(AKTIVITET_ID)
-                .egenAktivitetData(new EgenAktivitetData());
-
-        val aktivitet = aktivitetBuilder.build();
-        when(aktivitetDAO.hentAktivitet(AKTIVITET_ID)).thenReturn(aktivitet);
+        val aktivitet = lagEnNyAktivitet();
+        mockHentAktivitet(aktivitet);
 
         val avsluttKommentar = "Alexander er best";
         val nyStatus = AktivitetStatus.GJENNOMFORT;
-        val oppdatertAktivitet = aktivitetBuilder
+        val oppdatertAktivitet = aktivitet
+                .toBuilder()
                 .beskrivelse("ikke rett beskrivelse")
                 .avsluttetKommentar(avsluttKommentar)
                 .status(nyStatus)
                 .build();
         aktivitetService.oppdaterStatus(oppdatertAktivitet);
 
-        Mockito.verify(aktivitetDAO, times(1)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet = ((AktivitetData) argumentCaptor.getValue());
-
-        assertThat(capturedAktiviet.getBeskrivelse(), equalTo(aktivitet.getBeskrivelse()));
-
-        assertThat(capturedAktiviet.getStatus(), equalTo(nyStatus));
-        assertThat(capturedAktiviet.getAvsluttetKommentar(), equalTo(avsluttKommentar));
+        captureInsertAktivitetArgument();
+        assertThat(getCapturedAktivitet().getBeskrivelse(), equalTo(aktivitet.getBeskrivelse()));
+        assertThat(getCapturedAktivitet().getStatus(), equalTo(nyStatus));
+        assertThat(getCapturedAktivitet().getAvsluttetKommentar(), equalTo(avsluttKommentar));
     }
 
     @Test
     public void oppdaterEtikett() {
-        val stilling = nyttStillingssøk();
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(JOBBSOEKING)
-                .id(AKTIVITET_ID)
-                .stillingsSoekAktivitetData(stilling);
+        val aktivitet = lagEnNyAktivitet();
+        mockHentAktivitet(aktivitet);
 
-        val aktivitet = aktivitetBuilder.build();
-        when(aktivitetDAO.hentAktivitet(AKTIVITET_ID)).thenReturn(aktivitet);
-
-        val oppdatertAktivitet = aktivitetBuilder
+        val oppdatertAktivitet = aktivitet
+                .toBuilder()
                 .beskrivelse("Alexander er fremdeles best")
-                .stillingsSoekAktivitetData(stilling.setStillingsoekEtikett(StillingsoekEtikettData.AVSLAG))
+                .stillingsSoekAktivitetData(aktivitet
+                        .getStillingsSoekAktivitetData()
+                        .setStillingsoekEtikett(StillingsoekEtikettData.AVSLAG))
                 .build();
         aktivitetService.oppdaterEtikett(oppdatertAktivitet);
 
-        Mockito.verify(aktivitetDAO, times(1)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet = ((AktivitetData) argumentCaptor.getValue());
-
-        assertThat(capturedAktiviet.getBeskrivelse(), equalTo(aktivitet.getBeskrivelse()));
-
-        assertThat(capturedAktiviet.getStillingsSoekAktivitetData().getStillingsoekEtikett(),
+        captureInsertAktivitetArgument();
+        assertThat(getCapturedAktivitet().getBeskrivelse(), equalTo(aktivitet.getBeskrivelse()));
+        assertThat(getCapturedAktivitet().getStillingsSoekAktivitetData().getStillingsoekEtikett(),
                 equalTo(StillingsoekEtikettData.AVSLAG));
     }
 
     @Test
     public void oppdaterAktivitetFrist() {
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .id(AKTIVITET_ID)
-                .egenAktivitetData(new EgenAktivitetData());
-
-        val aktivitet = aktivitetBuilder.build();
+        val aktivitet = lagEnNyAktivitet();
 
         val nyFrist = new Date();
         aktivitetService.oppdaterAktivitetFrist(aktivitet, nyFrist);
 
-        Mockito.verify(aktivitetDAO, times(1)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet = ((AktivitetData) argumentCaptor.getValue());
-
-        assertThat(capturedAktiviet.getTilDato(), equalTo(nyFrist));
+        captureInsertAktivitetArgument();
+        assertThat(getCapturedAktivitet().getTilDato(), equalTo(nyFrist));
     }
 
     @Test
     public void oppdaterAktivitet() {
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .egenAktivitetData(new EgenAktivitetData())
-                .id(AKTIVITET_ID);
-
-        val nyBeskrivelse = "Alexander er den beste";
-        val nyLenke = "www.alexander-er-best.no";
-        val oppdatertAktivitet = aktivitetBuilder
-                .beskrivelse(nyBeskrivelse)
-                .lenke(nyLenke)
+        val aktivitet = lagEnNyAktivitet();
+        val oppdatertAktivitet = aktivitet
+                .toBuilder()
+                .beskrivelse("Alexander er den beste")
+                .lenke("www.alexander-er-best.no")
                 .build();
-        aktivitetService.oppdaterAktivitet(aktivitetBuilder.build(), oppdatertAktivitet);
+        aktivitetService.oppdaterAktivitet(aktivitet, oppdatertAktivitet);
 
-        Mockito.verify(aktivitetDAO, times(1)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet = ((AktivitetData) argumentCaptor.getValue());
-
-        assertThat(capturedAktiviet.getBeskrivelse(), equalTo(nyBeskrivelse));
-        assertThat(capturedAktiviet.getLenke(), equalTo(nyLenke));
+        captureInsertAktivitetArgument();
+        assertThat(getCapturedAktivitet().getBeskrivelse(), equalTo(oppdatertAktivitet.getBeskrivelse()));
+        assertThat(getCapturedAktivitet().getLenke(), equalTo(oppdatertAktivitet.getLenke()));
     }
 
     @Test(expected = VersjonsKonflikt.class)
     public void oppdaterAktivitet_skal_gi_versjonsKonflikt_hvis_to_oppdaterer_aktiviteten_samtidig() {
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .egenAktivitetData(new EgenAktivitetData())
-                .id(AKTIVITET_ID);
-
-        val aktivitet = aktivitetBuilder.build();
-
+        val aktivitet = lagEnNyAktivitet();
         doThrow(new DuplicateKeyException("versjon fins")).when(aktivitetDAO).insertAktivitet(any());
-
         aktivitetService.oppdaterAktivitet(aktivitet, aktivitet);
     }
 
     @Test
     public void oppdaterAktivitet_skal_sette_rett_transaksjonstype() {
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .egenAktivitetData(new EgenAktivitetData())
-                .id(AKTIVITET_ID);
+        val aktivitet = lagEnNyAktivitet();
 
-        val nyBeskrivelse = "Alexander er den beste";
-        val nyLenke = "www.alexander-er-best.no";
-        val oppdatertAktivitet = aktivitetBuilder
-                .beskrivelse(nyBeskrivelse)
-                .lenke(nyLenke);
-        aktivitetService.oppdaterAktivitet(aktivitetBuilder.build(), oppdatertAktivitet.build());
+        aktivitetService.oppdaterAktivitet(aktivitet, aktivitet);
 
-        Mockito.verify(aktivitetDAO, times(1)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet = ((AktivitetData) argumentCaptor.getValue());
+        captureInsertAktivitetArgument();
+        assertThat(getCapturedAktivitet().getTransaksjonsTypeData(), equalTo(TransaksjonsTypeData.DETALJER_ENDRET));
 
-        assertThat(capturedAktiviet.getTransaksjonsTypeData(), equalTo(TransaksjonsTypeData.DETALJER_ENDRET));
-
-        aktivitetService.oppdaterAktivitet(aktivitetBuilder.build(), oppdatertAktivitet.avtalt(true).build());
-        Mockito.verify(aktivitetDAO, times(2)).insertAktivitet((AktivitetData) argumentCaptor.capture());
-        val capturedAktiviet2 = ((AktivitetData) argumentCaptor.getValue());
-        assertThat(capturedAktiviet2.getTransaksjonsTypeData(), equalTo(TransaksjonsTypeData.AVTALT));
+        aktivitetService.oppdaterAktivitet(aktivitet, aktivitet.toBuilder().avtalt(true).build());
+        captureInsertAktivitetArgument();
+        assertThat(getCapturedAktivitet().getTransaksjonsTypeData(), equalTo(TransaksjonsTypeData.AVTALT));
     }
 
 
     @Test
     public void skal_ikke_kunne_endre_aktivitet_nar_den_er_avbrutt_eller_fullfort() {
-        val aktivitetBuilder = nyAktivitet()
-                .aktivitetType(EGENAKTIVITET)
-                .egenAktivitetData(new EgenAktivitetData())
-                .status(AktivitetStatus.AVBRUTT)
-                .id(AKTIVITET_ID);
-        when(aktivitetDAO.hentAktivitet(AKTIVITET_ID)).thenReturn(aktivitetBuilder.build());
+        val aktivitet = lagEnNyAktivitet().toBuilder().status(AktivitetStatus.AVBRUTT).build();
+        mockHentAktivitet(aktivitet);
 
         try {
-            aktivitetService.oppdaterStatus(aktivitetBuilder.build());
+            aktivitetService.oppdaterStatus(aktivitet);
             fail();
         } catch (IllegalArgumentException ignored) {
         }
         try {
-            aktivitetService.oppdaterEtikett(aktivitetBuilder.build());
+            aktivitetService.oppdaterEtikett(aktivitet);
             fail();
         } catch (IllegalArgumentException ignored) {
         }
         try {
-            aktivitetService.oppdaterAktivitetFrist(aktivitetBuilder.build(), new Date());
+            aktivitetService.oppdaterAktivitetFrist(aktivitet, new Date());
             fail();
         } catch (IllegalArgumentException ignored) {
         }
         try {
-            aktivitetService.oppdaterAktivitet(aktivitetBuilder.build(), aktivitetBuilder.build());
+            aktivitetService.oppdaterAktivitet(aktivitet, aktivitet);
             fail();
         } catch (IllegalArgumentException ignored) {
         }
 
         verify(aktivitetDAO, never()).insertAktivitet(any());
 
+    }
+
+    public AktivitetData lagEnNyAktivitet() {
+        val stilling = nyttStillingssøk();
+        return nyAktivitet()
+                .aktivitetType(JOBBSOEKING)
+                .id(AKTIVITET_ID)
+                .stillingsSoekAktivitetData(stilling)
+                .build();
+    }
+
+    public void mockHentAktivitet(AktivitetData aktivitetData) {
+        when(aktivitetDAO.hentAktivitet(AKTIVITET_ID)).thenReturn(aktivitetData);
+    }
+
+    public void captureInsertAktivitetArgument() {
+        Mockito.verify(aktivitetDAO, atLeastOnce()).insertAktivitet((AktivitetData) argumentCaptor.capture());
+    }
+
+    public AktivitetData getCapturedAktivitet() {
+        return ((AktivitetData) argumentCaptor.getValue());
     }
 
 }
