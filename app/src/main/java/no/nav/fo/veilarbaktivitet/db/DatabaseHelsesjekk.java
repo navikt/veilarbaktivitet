eb125e6dca2a35e92c2292a2a8cfa6adb8d7d304
Steffen Hageland <steffen.hageland@nav.no>
2017-06-23 10:15:40 +0200

    Ta i bruk ny versjon av helsesjekken

--- app/src/main/java/no/nav/fo/veilarbaktivitet/db/DatabaseHelsesjekk.java
+++ app/src/main/java/no/nav/fo/veilarbaktivitet/db/DatabaseHelsesjekk.java
@@ -1,10 +1,14 @@
 package no.nav.fo.veilarbaktivitet.db;
 
-import no.nav.fo.veilarbaktivitet.Helsesjekk;
+import no.nav.apiapp.selftest.Helsesjekk;
+import no.nav.apiapp.selftest.HelsesjekkMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.sql.SQLException;
 
 @Component
 public class DatabaseHelsesjekk implements Helsesjekk {
@@ -12,9 +16,26 @@ public class DatabaseHelsesjekk implements Helsesjekk {
     @Inject
     private JdbcTemplate jdbcTemplate;
 
+    Logger logger = LoggerFactory.getLogger(DatabaseHelsesjekk.class);
+
     @Override
     public void helsesjekk() {
         jdbcTemplate.queryForObject("SELECT 1 FROM DUAL", Long.class);
+    }
+
+    @Override
+    public HelsesjekkMetadata getMetadata() {
+        String dbUri = "Ukjent uri";
+        try {
+            dbUri = jdbcTemplate.getDataSource().getConnection().getMetaData().getURL();
+        } catch (SQLException e) {
+            logger.error("Kunne ikke hente connection for datasource.", e);
+        }
+        return new HelsesjekkMetadata(
+                "Database: " + dbUri,
+                "Lokal database for VeilArbAktivitet",
+                true
+        );
     }
 
 }
