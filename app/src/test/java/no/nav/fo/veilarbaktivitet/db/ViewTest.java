4e234b4f41a2b48ff02b217340d6a58c5eb422a0
Johan Rusvik <johan.rusvik@nav.no>
2017-06-29 10:09:21 +0200

    Genererer 1:1 views for et bestemt sett av tabeller

---
+++ app/src/test/java/no/nav/fo/veilarbaktivitet/db/ViewTest.java
@@ -0,0 +1,59 @@
+package no.nav.fo.veilarbaktivitet.db;
+
+import no.nav.apiapp.util.JsonUtils;
+import no.nav.fo.IntegrasjonsTest;
+import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.inject.Inject;
+import java.io.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ViewTest extends IntegrasjonsTest {
+
+    private final String[] views = new String[] {"DVH_TRANSAKSJON_TYPE", "DVH_AKTIVITET_LIVSLOPSTATUS_TYPE", "DVH_AKTIVITET_TYPE", "DVH_STILLINGSOK_ETIKETT_TYPE", "DVH_AKTIVITET", "DVH_STILLINGSOK_AKTIVITET", "DVH_SOKEAVTALE_AKTIVITET", "DVH_EGENAKTIVITET"};
+
+    @Inject
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void views_eksisterer() {
+        for(String viewName : views) {
+            List<Map<String, Object>> view = jdbcTemplate.queryForList("SELECT * FROM " + viewName + ";");
+            assertThat(view).isNotNull();
+        }
+    }
+
+    @Test
+    public void view_reflekterer_tabell_kolonner() throws IOException {
+
+        for(String view : views) {
+
+            String kolonneData = JsonUtils.toJson(getKolonneDataForView(view));
+
+            String kolonneDataFasit = readContentFromFile("view-meta-data/" + view.toLowerCase() + ".json");
+
+            assertThat(kolonneData).isEqualTo(kolonneDataFasit);
+        }
+
+    }
+
+    private List<Map<String, Object>> getKolonneDataForView(String view) {
+        return jdbcTemplate.queryForList(
+            "SELECT " +
+                "COLUMN_NAME, " +
+                "TYPE_NAME, " +
+                "CHARACTER_MAXIMUM_LENGTH " +
+                "FROM INFORMATION_SCHEMA.COLUMNS " +
+                "WHERE TABLE_NAME = '" + view + "';"
+        );
+    }
+
+    public String readContentFromFile(String fileName) {
+        return new Scanner(getClass().getClassLoader().getResourceAsStream(fileName), "UTF-8").useDelimiter("\\A").next();
+    }
+}
