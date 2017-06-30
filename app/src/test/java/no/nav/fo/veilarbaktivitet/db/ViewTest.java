cb9d77289152c2dc21da65d925cc8ade1990043e
Johan Rusvik <johan.rusvik@nav.no>
2017-06-30 10:12:46 +0200

    Refaktorerer til å bruke parameterisert junit test

--- app/src/test/java/no/nav/fo/veilarbaktivitet/db/ViewTest.java
+++ app/src/test/java/no/nav/fo/veilarbaktivitet/db/ViewTest.java
@@ -1,49 +1,71 @@
 package no.nav.fo.veilarbaktivitet.db;
 
 import no.nav.apiapp.util.JsonUtils;
+import no.nav.fo.DatabaseTestContext;
 import no.nav.fo.IntegrasjonsTest;
+import org.json.JSONArray;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.inject.Inject;
-import java.io.*;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(value = Parameterized.class)
 public class ViewTest extends IntegrasjonsTest {
 
-    private final String[] views = new String[] {"DVH_TRANSAKSJON_TYPE", "DVH_AKTIVITET_LIVSLOPSTATUS_TYPE", "DVH_AKTIVITET_TYPE", "DVH_STILLINGSOK_ETIKETT_TYPE", "DVH_AKTIVITET", "DVH_STILLINGSOK_AKTIVITET", "DVH_SOKEAVTALE_AKTIVITET", "DVH_EGENAKTIVITET"};
+    private static final String[] views = new String[]{
+        "DVH_TRANSAKSJON_TYPE",
+        "DVH_AKTIVITET_LIVSLOPSTATUS_TYPE",
+        "DVH_AKTIVITET_TYPE",
+        "DVH_STILLINGSOK_ETIKETT_TYPE",
+        "DVH_AKTIVITET",
+        "DVH_STILLINGSOK_AKTIVITET",
+        "DVH_SOKEAVTALE_AKTIVITET",
+        "DVH_EGENAKTIVITET"
+    };
 
     @Inject
     private JdbcTemplate jdbcTemplate;
 
-    @Test
-    public void views_eksisterer() {
-        for(String view : views) {
-            List<Map<String, Object>> viewData = jdbcTemplate.queryForList("SELECT * FROM " + view + ";");
-            assertThat(viewData).isNotNull();
-        }
+    @Parameter(value = 0)
+    public String viewName;
+
+    @Parameters(name = "{0}")
+    public static Object[] data() {
+        Object[] objects = Arrays.stream(views).map(view -> new Object[]{
+            view,
+        }).toArray();
+
+        return objects;
     }
 
     @Test
-    public void view_reflekterer_tabell_kolonner() throws IOException {
+    public void view_eksisterer() {
+        List<Map<String, Object>> viewData = jdbcTemplate.queryForList("SELECT * FROM " + viewName + ";");
 
-        for(String view : views) {
-
-            String kolonneData = JsonUtils.toJson(hentKolonneDataForView(view));
-
-            String kolonneDataFasit = lesInnholdFraFil("view-meta-data/" + view.toLowerCase() + ".json");
-
-            assertThat(kolonneData).isEqualTo(kolonneDataFasit);
-        }
-
+        assertThat(viewData).isNotNull();
     }
 
-    private List<Map<String, Object>> hentKolonneDataForView(String view) {
-        return jdbcTemplate.queryForList(
+    @Test
+    public void view_skal_reflektere_kolonner_i_tabell() {
+        String kolonneData = jsonFormatter(JsonUtils.toJson(hentKolonneDataForView(viewName)));
+        String kolonneDataFasit = jsonFormatter(lesInnholdFraFil("view-meta-data/" + viewName.toLowerCase() + ".json"));
+
+        assertThat(kolonneData).isEqualTo(kolonneDataFasit);
+    }
+
+    private static String jsonFormatter(String jsonArray) {
+        return new JSONArray(jsonArray).toString();
+    }
+
+    private static List<Map<String, Object>> hentKolonneDataForView(String view) {
+        return new JdbcTemplate(DatabaseTestContext.buildDataSource()).queryForList(
             "SELECT " +
                 "COLUMN_NAME, " +
                 "TYPE_NAME, " +
@@ -53,7 +75,7 @@ public class ViewTest extends IntegrasjonsTest {
         );
     }
 
-    public String lesInnholdFraFil(String filNavn) {
-        return new Scanner(getClass().getClassLoader().getResourceAsStream(filNavn), "UTF-8").useDelimiter("\\A").next();
+    private static String lesInnholdFraFil(String filNavn) {
+        return new Scanner(ViewTest.class.getClassLoader().getResourceAsStream(filNavn), "UTF-8").useDelimiter("\\A").next();
     }
 }
