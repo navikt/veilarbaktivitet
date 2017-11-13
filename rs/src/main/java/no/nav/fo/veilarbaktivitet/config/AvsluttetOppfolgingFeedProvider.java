35a1277f503b9e4782b1e864637b0f980795425c
alex <alexander.svendsen@nav.no>
2017-11-13 14:03:59 +0100

    Masternode er eneste som skal lese fra feed

--- rs/src/main/java/no/nav/fo/veilarbaktivitet/config/AvsluttetOppfolgingFeedProvider.java
+++ rs/src/main/java/no/nav/fo/veilarbaktivitet/config/AvsluttetOppfolgingFeedProvider.java
@@ -32,18 +32,18 @@ public class AvsluttetOppfolgingFeedProvider {
     }
 
     public void lesAvsluttetOppfolgingFeed(String lastEntryId, List<AvsluttetOppfolgingFeedDTO> elements) {
-        Date lastSuccessfulId = null;
-        for (AvsluttetOppfolgingFeedDTO element : elements) {
-            aktivitetService.settAktiviteterTilHistoriske(element.getAktoerid(), element.getSluttdato());
-            lastSuccessfulId = element.getOppdatert();
-        }
 
-        // Håndterer ikke exceptions her. Dersom en exception oppstår i løkkeprosesseringen over, vil 
+        // Håndterer ikke exceptions her. Dersom en exception oppstår i løkkeprosesseringen over, vil
         // vi altså IKKE få oppdatert siste id. Dermed vil vi lese feeden på nytt fra siste kjente id og potensielt
         // prosessere noen elementer flere ganger. Dette skal gå bra, siden koden som setter dialoger til historisk
         // er idempotent
-        if(lastSuccessfulId != null) {
-            avsluttetOppfolgingFeedDAO.oppdaterSisteFeedId(lastSuccessfulId);
-        }        
+
+        elements.stream()
+                .map(element -> {
+                    aktivitetService.settAktiviteterTilHistoriske(element.getAktoerid(), element.getSluttdato());
+                    return element.getOppdatert();
+                })
+                .reduce((a1, a2) -> a2)
+                .ifPresent(lastSuccessfulId -> avsluttetOppfolgingFeedDAO.oppdaterSisteFeedId(lastSuccessfulId));
     }
 }
