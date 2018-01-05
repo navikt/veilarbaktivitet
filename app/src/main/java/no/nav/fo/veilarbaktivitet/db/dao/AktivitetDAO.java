ee63c5278ffcc534a1a46d8faa98c226cc6ddb5f
Håkon Planke Holm <hakon.planke.holm@nav.no>
2018-01-05 13:05:07 +0100

    [FO-223] lag til transactional på insertAktivitet bugfix

--- app/src/main/java/no/nav/fo/veilarbaktivitet/db/dao/AktivitetDAO.java
+++ app/src/main/java/no/nav/fo/veilarbaktivitet/db/dao/AktivitetDAO.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import org.springframework.transaction.annotation.Transactional;
 
+
 import javax.inject.Inject;
 import java.util.Date;
 import java.util.List;
