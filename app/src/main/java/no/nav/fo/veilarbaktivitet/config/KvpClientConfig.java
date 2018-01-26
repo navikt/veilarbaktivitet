bcc247e0cc144dfa3dda44a2e7e635ba8b3cfd37
Kim Tore Jensen <ambientsound@noreply.users.github.com>
2018-01-26 15:41:20 +0100

    FO-112: use veilarboppfolging API endpoint instead of custom KVP endpoint.

--- app/src/main/java/no/nav/fo/veilarbaktivitet/config/KvpClientConfig.java
+++ app/src/main/java/no/nav/fo/veilarbaktivitet/config/KvpClientConfig.java
@@ -10,7 +10,7 @@ import javax.ws.rs.client.Client;
 @Configuration
 public class KvpClientConfig {
 
-    private static final String KVP_ENDPOINT_URL = "kvp.endpoint.url";
+    private static final String VEILARBOPPFOLGING_API_URL = "veilarboppfolging.api.url";
     private static final String KVP_CONNECT_TIMEOUT = "kvp.connect.timeout.ms";
     private static final String KVP_READ_TIMEOUT = "kvp.read.timeout.ms";
 
@@ -21,6 +21,6 @@ public class KvpClientConfig {
         configBuilder.connectTimeout(Integer.parseInt(System.getProperty(KVP_CONNECT_TIMEOUT, "1000")));
         RestUtils.RestConfig config = configBuilder.build();
         Client client = RestUtils.createClient(config);
-        return new KvpClient(System.getProperty(KVP_ENDPOINT_URL), client);
+        return new KvpClient(System.getProperty(VEILARBOPPFOLGING_API_URL) + "/kvp", client);
     }
 }
