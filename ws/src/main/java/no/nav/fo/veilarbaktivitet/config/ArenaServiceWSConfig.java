88c018522360a32d7661984c7fb0ac819d3368b7
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-07-10 16:08:45 +0200

    PUS-14 bump common

--- ws/src/main/java/no/nav/fo/veilarbaktivitet/config/ArenaServiceWSConfig.java
+++ ws/src/main/java/no/nav/fo/veilarbaktivitet/config/ArenaServiceWSConfig.java
@@ -1,18 +1,17 @@
 package no.nav.fo.veilarbaktivitet.config;
 
-import no.nav.modig.security.ws.UserSAMLOutInterceptor;
+import no.nav.sbl.dialogarena.common.cxf.SamlPropagatingOutInterceptor;
 import no.nav.tjeneste.virksomhet.tiltakogaktivitet.v1.binding.TiltakOgAktivitetV1;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ArenaServiceWSConfig {
 
     @Bean
     public TiltakOgAktivitetV1 tiltakOgAktivitetV1() {
         return ArenaServiceConfig.tiltakOgAktivitetV1Client()
-                .withOutInterceptor(new UserSAMLOutInterceptor())
+                .withOutInterceptor(new SamlPropagatingOutInterceptor())
                 .build();
     }
 
