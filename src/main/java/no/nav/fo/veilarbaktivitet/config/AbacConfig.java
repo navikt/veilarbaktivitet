ee9e590e141343c1ccaa7fce21ba56e3024c29a6
matiasvinjevoll <matias.vinjevoll@nav.no>
2020-03-03 14:58:17 +0100

    FO-2599 bytte til PepClient og tilgangssjekk på aktør id

--- src/main/java/no/nav/fo/veilarbaktivitet/config/AbacConfig.java
+++ src/main/java/no/nav/fo/veilarbaktivitet/config/AbacConfig.java
@@ -1,37 +1,19 @@
 package no.nav.fo.veilarbaktivitet.config;
 
-import no.nav.apiapp.security.veilarbabac.VeilarbAbacPepClient;
-import no.nav.brukerdialog.security.oidc.SystemUserTokenProvider;
+import no.nav.apiapp.security.PepClient;
 import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.ResourceType;
-import no.nav.sbl.featuretoggle.unleash.UnleashService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.inject.Inject;
 
 @Configuration
 @Import({AbacContext.class})
 public class AbacConfig {
 
-    private SystemUserTokenProvider systemUserTokenProvider = new SystemUserTokenProvider();
-
-    @Inject
-    UnleashService unleashService;
-
     @Bean
-    public VeilarbAbacPepClient pepClient(Pep pep) {
-
-        return VeilarbAbacPepClient.ny()
-                .medPep(pep)
-                .medSystemUserTokenProvider(()->systemUserTokenProvider.getToken())
-                .brukAktoerId(()->unleashService.isEnabled("veilarbaktivitet.veilarbabac.aktor"))
-                .sammenlikneTilgang(()->unleashService.isEnabled("veilarbaktivitet.veilarbabac.sammenlikn"))
-                .foretrekkVeilarbAbacResultat(()->unleashService.isEnabled("veilarbaktivitet.veilarbabac.foretrekk_veilarbabac"))
-                .bygg();
+    public PepClient pepClient(Pep pep) {
+        return new PepClient(pep, "veilarb", ResourceType.VeilArbPerson);
     }
-
 }
