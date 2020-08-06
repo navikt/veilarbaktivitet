87a20610e2e681de931c1a2abbb3cf40af7e24b7
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-08-06 13:38:16 +0200

    Fix MigrerDatabaseTest

--- src/test/java/no/nav/veilarbaktivitet/db/MigrerDatabaseTest.java
+++ src/test/java/no/nav/veilarbaktivitet/db/MigrerDatabaseTest.java
@@ -1,19 +1,15 @@
 package no.nav.veilarbaktivitet.db;
 
-import no.nav.veilarbaktivitet.mock.LocalH2Database;
 import org.junit.Test;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertThat;
 
-public class MigrerDatabaseTest extends DatabaseTest {
-
-    private final JdbcTemplate jdbcTemplate = LocalH2Database.getDb();
+public class MigrerDatabaseTest extends IsolatedDatabaseTest {
 
     @Test
     public void kanQueryeDatabasen() {
-        assertThat(jdbcTemplate.queryForList("SELECT * FROM AKTIVITET"), empty());
+        assertThat(db.queryForList("SELECT * FROM AKTIVITET"), empty());
     }
 
 }
