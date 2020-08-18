047e9fabf3ec97c61d084f4f536ac1bdca1ecc21
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-08-18 13:49:00 +0200

    Use SystemUser token in kvp client

--- src/main/java/no/nav/veilarbaktivitet/client/KvpClientImpl.java
+++ src/main/java/no/nav/veilarbaktivitet/client/KvpClientImpl.java
@@ -1,14 +1,11 @@
 package no.nav.veilarbaktivitet.client;
 
-import no.nav.common.auth.subject.SsoToken;
-import no.nav.common.auth.subject.SubjectHandler;
 import no.nav.common.rest.client.RestUtils;
 import no.nav.veilarbaktivitet.domain.KvpDTO;
 import no.nav.veilarbaktivitet.domain.Person;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -25,15 +22,10 @@ public class KvpClientImpl implements KvpClient {
         this.client = client;
     }
 
-    public static String createBearerToken() {
-        return SubjectHandler.getSsoToken().map(SsoToken::getToken).map(token -> "Bearer " + token).orElse(null);
-    }
-
     public KvpDTO get(Person.AktorId aktorId) {
         String uri = String.format("%s/kvp/%s/currentStatus", baseUrl, aktorId.get());
         Request request = new Request.Builder()
                 .url(uri)
-                .header(HttpHeaders.AUTHORIZATION, createBearerToken())
                 .build();
         try (Response response = client.newCall(request).execute()) {
             RestUtils.throwIfNotSuccessful(response);
