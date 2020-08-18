8d4e8b387277b8e246ba466d817446ea759d62d3
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-08-18 12:32:05 +0200

    Configure arena helsesjekk with SystemUser

--- src/main/java/no/nav/veilarbaktivitet/helsesjekk/ArenaServiceHelsesjekk.java
+++ src/main/java/no/nav/veilarbaktivitet/helsesjekk/ArenaServiceHelsesjekk.java
@@ -1,8 +1,13 @@
 package no.nav.veilarbaktivitet.helsesjekk;
 
+import no.nav.common.cxf.CXFClient;
+import no.nav.common.cxf.StsConfig;
 import no.nav.common.health.HealthCheck;
 import no.nav.common.health.HealthCheckResult;
+import no.nav.common.utils.Credentials;
 import no.nav.tjeneste.virksomhet.tiltakogaktivitet.v1.binding.TiltakOgAktivitetV1;
+import no.nav.veilarbaktivitet.config.ApplicationContext;
+import no.nav.veilarbaktivitet.config.EnvironmentProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +20,18 @@ public class ArenaServiceHelsesjekk implements HealthCheck {
     private final TiltakOgAktivitetV1 tiltakOgAktivitetV1;
 
     @Autowired
-    public ArenaServiceHelsesjekk(TiltakOgAktivitetV1 tiltakOgAktivitetV1){
-        this.tiltakOgAktivitetV1 = tiltakOgAktivitetV1;
+    public ArenaServiceHelsesjekk(EnvironmentProperties properties, Credentials credentials) {
+        StsConfig stsConfig = StsConfig.builder()
+                .url(properties.getCxfStsUrl())
+                .username(credentials.username)
+                .password(credentials.password)
+                .build();
+
+        this.tiltakOgAktivitetV1 = new CXFClient<>(TiltakOgAktivitetV1.class)
+                .address(getRequiredProperty(ApplicationContext.VIRKSOMHET_TILTAKOGAKTIVITET_V1_ENDPOINTURL_PROPERTY))
+                .configureStsForSystemUser(stsConfig)
+                .build();
+
     }
 
     @Override
