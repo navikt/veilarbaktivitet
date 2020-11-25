df70d1bf95870cc479e4ba0d50e9dbc21c9bc168
Cato Olsen <cato.olsen@nav.no>
2020-11-25 14:07:40 +0100

    Try to make this work. Highlights some issues with old code, which isn't especially testable in a Spring Boot context.

---
+++ src/test/java/no/nav/veilarbaktivitet/SpringBootApplicationContextTest.java
@@ -0,0 +1,20 @@
+package no.nav.veilarbaktivitet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Challenge: Make this work.
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class SpringBootApplicationContextTest {
+
+    @Test
+    public void shouldRunWithoutExceptions() {
+        // Does nothing.
+    }
+
+}
