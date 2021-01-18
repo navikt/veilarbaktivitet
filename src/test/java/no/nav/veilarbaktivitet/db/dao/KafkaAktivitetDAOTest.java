db81d024dc5fa718efe53d5f940f5cedf03c9ee8
stianStensli <stian.horn.stensli@nav.no>
2021-01-18 13:18:25 +0100

    Tc-220 ryddet opp i, og lagt til test i KafkaAktivitetDAOTest

--- src/test/java/no/nav/veilarbaktivitet/db/dao/KafkaAktivitetDAOTest.java
+++ src/test/java/no/nav/veilarbaktivitet/db/dao/KafkaAktivitetDAOTest.java
@@ -2,22 +2,21 @@ package no.nav.veilarbaktivitet.db.dao;
 
 import lombok.val;
 import no.nav.veilarbaktivitet.aktiviterTilKafka.KafkaAktivitetDAO;
+import no.nav.veilarbaktivitet.aktiviterTilKafka.KafkaAktivitetMeldingV4;
 import no.nav.veilarbaktivitet.db.Database;
 import no.nav.veilarbaktivitet.db.DbTestUtils;
 import no.nav.veilarbaktivitet.domain.AktivitetData;
 import no.nav.veilarbaktivitet.domain.Person;
 import no.nav.veilarbaktivitet.mock.LocalH2Database;
 import no.nav.veilarbaktivitet.testutils.AktivitetDataTestBuilder;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.junit.Assert.assertThat;
 
 public class KafkaAktivitetDAOTest {
 
@@ -36,35 +35,51 @@ public class KafkaAktivitetDAOTest {
 
 
     @Test
-    public void opprette_og_hente_melding_som_ikke_er_sendt() {
-        val aktivitet = gitt_at_det_finnes_en_egen_aktivitet();
+    public void hentOppTil5000MeldingerSomIkkeErSendt_AktivitetErSendOgIkkeSendt_returnererKunDeUsendte() {
+        val usendt_1 = opprettEgenaktivitet();
+        val usendt_2 = opprettEgenaktivitet();
+        val usendt_3 = opprettEgenaktivitet();
+        val sendt_1 = opprettEgenaktivitet();
 
-        val aktiviteter = aktivitetDAO.hentAktiviteterForAktorId(AKTOR_ID);
-        assertThat(aktiviteter, hasSize(1));
-        assertThat(aktivitet, equalTo(aktiviteter.get(0)));
-        val usendteAktiviteter = kafkaAktivitetDAO.hentOppTil5000MeldingerSomIkkeErSendt();
-        assertThat(usendteAktiviteter, hasSize(1));
+        val usendteAktiviteter_pre = kafkaAktivitetDAO.hentOppTil5000MeldingerSomIkkeErSendt();
+        Assert.assertTrue(containsVersion(usendteAktiviteter_pre, usendt_1));
+        Assert.assertTrue(containsVersion(usendteAktiviteter_pre, usendt_2));
+        Assert.assertTrue(containsVersion(usendteAktiviteter_pre, usendt_3));
+        Assert.assertTrue(containsVersion(usendteAktiviteter_pre, sendt_1));
 
+        kafkaAktivitetDAO.updateSendtPaKafka(sendt_1.getVersjon(), 1L);
+        val usendteAktiviteter_post = kafkaAktivitetDAO.hentOppTil5000MeldingerSomIkkeErSendt();
+        Assert.assertTrue(containsVersion(usendteAktiviteter_post, usendt_1));
+        Assert.assertTrue(containsVersion(usendteAktiviteter_post, usendt_2));
+        Assert.assertTrue(containsVersion(usendteAktiviteter_post, usendt_3));
+        Assert.assertFalse(containsVersion(usendteAktiviteter_post, sendt_1));
     }
 
     @Test
-    public void marker_melding_som_sendt() {
-        val aktivitet = gitt_at_det_finnes_en_egen_aktivitet();
-
-        val aktiviteter = aktivitetDAO.hentAktiviteterForAktorId(AKTOR_ID);
-        assertThat(aktiviteter, hasSize(1));
-        assertThat(aktivitet, equalTo(aktiviteter.get(0)));
+    public void hentOppTil5000MeldingerSomIkkeErSendt_AktivitetErIkkeSendt_returnererEnAktivitet() {
+        val aktivitet = opprettEgenaktivitet();
         val usendteAktiviteter = kafkaAktivitetDAO.hentOppTil5000MeldingerSomIkkeErSendt();
-        assertThat(usendteAktiviteter, hasSize(1));
+        Assert.assertTrue(containsVersion(usendteAktiviteter, aktivitet));
+    }
 
-        kafkaAktivitetDAO.updateSendtPaKafka(usendteAktiviteter.get(0).getVersion(), 1L);
+    @Test
+    public void hentOppTil5000MeldingerSomIkkeErSendt_kunSendteAktiviteter_returnererIngen() {
+        val aktivitet = opprettEgenaktivitet();
+        val usendteAktiviteter = kafkaAktivitetDAO.hentOppTil5000MeldingerSomIkkeErSendt();
+        Assert.assertTrue(containsVersion(usendteAktiviteter, aktivitet));
 
+        kafkaAktivitetDAO.updateSendtPaKafka(aktivitet.getVersjon(), 1L);
         val usendteAktiviteter_tom = kafkaAktivitetDAO.hentOppTil5000MeldingerSomIkkeErSendt();
-        assertThat(usendteAktiviteter_tom, hasSize(0));
+        Assert.assertFalse(containsVersion(usendteAktiviteter_tom, aktivitet));
 
     }
 
-    private AktivitetData gitt_at_det_finnes_en_egen_aktivitet() {
+    private boolean containsVersion(List<KafkaAktivitetMeldingV4> list, AktivitetData aktivitet){
+        return list.stream()
+                .filter(e -> e.getVersion().equals(aktivitet.getVersjon())).count() == 1;
+    }
+
+    private AktivitetData opprettEgenaktivitet() {
         val aktivitet = AktivitetDataTestBuilder.nyEgenaktivitet();
 
         return insertAktivitet(aktivitet);
