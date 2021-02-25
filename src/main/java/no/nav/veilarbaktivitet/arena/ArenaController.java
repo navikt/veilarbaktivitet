580d0f99340f97cc94328513504bec25f228c6d3
hakon planke holm <hakon.planke.holm@nav.no>
2021-02-25 11:42:28 +0100

    forsette arena fho

--- src/main/java/no/nav/veilarbaktivitet/arena/ArenaController.java
+++ src/main/java/no/nav/veilarbaktivitet/arena/ArenaController.java
@@ -1,12 +1,11 @@
 package no.nav.veilarbaktivitet.arena;
 
 import lombok.RequiredArgsConstructor;
-import no.nav.common.auth.subject.SubjectHandler;
 import no.nav.veilarbaktivitet.avtaltMedNav.Forhaandsorientering;
 import no.nav.veilarbaktivitet.domain.Person;
 import no.nav.veilarbaktivitet.domain.arena.ArenaAktivitetDTO;
 import no.nav.veilarbaktivitet.service.AuthService;
-import no.nav.veilarbaktivitet.service.BrukerService;
+import no.nav.veilarbaktivitet.service.UserInContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -16,34 +15,29 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Optional;
 
+import static no.nav.veilarbaktivitet.domain.AktivitetStatus.AVBRUTT;
+import static no.nav.veilarbaktivitet.domain.AktivitetStatus.FULLFORT;
+
 @Transactional
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/arena")
 public class ArenaController {
-
     private final ArenaForhaandsorienteringDAO forhaandsorienteringDAO;
-    private final HttpServletRequest requestProvider;
-    private final BrukerService brukerService;
+    private final UserInContext userInContext;
     private final AuthService authService;
     private final ArenaService arenaService;
 
     @PutMapping("/forhaandsorientering")
     public ArenaAktivitetDTO sendForhaandsorientering(@RequestBody Forhaandsorientering forhaandsorientering, @RequestParam String arenaaktivitetId) {
-
-        Person person = getContextUserIdent();
-        Person.AktorId aktorId = brukerService.getAktorIdForPerson(person).orElseThrow(RuntimeException::new);
-        Person.Fnr fnr = brukerService.getFNR(person).orElseThrow(RuntimeException::new);
+        Person.AktorId aktorId = userInContext.getAktorId().orElseThrow(RuntimeException::new);
         authService.sjekkTilgangOgInternBruker(aktorId.get(), null);
 
-        validerInput(forhaandsorientering);
+        getInputFeilmelding(forhaandsorientering)
+                .ifPresent( feilmelding -> {throw new ResponseStatusException(HttpStatus.BAD_REQUEST, feilmelding);});
 
-        List<ArenaAktivitetDTO> arenaAktiviteter = arenaService.hentAktiviteter(fnr);
-
-        ArenaAktivitetDTO arenaAktivitet = arenaAktiviteter
-                .stream()
-                .filter(arenaAktivitetDTO -> arenaAktivitetDTO.getId().equals(arenaaktivitetId))
-                .findAny()
+        Person.Fnr fnr = userInContext.getFnr().orElseThrow(RuntimeException::new);
+        ArenaAktivitetDTO arenaAktivitet = arenaService.hentAktivitet(fnr, arenaaktivitetId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Aktiviteten eksisterer ikke"));
 
         if (arenaAktivitet.getForhaandsorientering() != null) {
@@ -55,27 +49,39 @@ public class ArenaController {
         return arenaAktivitet.setForhaandsorientering(forhaandsorientering);
     }
 
-    private Person getContextUserIdent() {
-        if (BrukerService.erEksternBruker()) {
-            return SubjectHandler.getIdent().map(Person::fnr).orElseThrow(RuntimeException::new);
-        }
+    @GetMapping("/tiltak")
+    public List<ArenaAktivitetDTO> hentArenaAktiviteter() {
 
-        Optional<Person> fnr = Optional.ofNullable(requestProvider.getParameter("fnr")).map(Person::fnr);
-        Optional<Person> aktorId = Optional.ofNullable(requestProvider.getParameter("aktorId")).map(Person::aktorId);
-        return fnr.orElseGet(() -> aktorId.orElseThrow(RuntimeException::new));
+        Person.Fnr fnr = userInContext.getFnr().orElseThrow(RuntimeException::new);
+        authService.sjekkTilgangTilPerson(fnr);
+
+        return arenaService.hentAktiviteter(fnr);
     }
 
-    private void validerInput(Forhaandsorientering forhaandsorientering) {
+    @GetMapping("/harTiltak")
+    public boolean hentHarTiltak() {
+        Person.Fnr fnr = userInContext.getFnr().orElseThrow(RuntimeException::new);
+        authService.sjekkTilgangTilPerson(fnr);
+
+        return arenaService.hentAktiviteter(fnr)
+                .stream()
+                .map(ArenaAktivitetDTO::getStatus)
+                .anyMatch(status -> status != AVBRUTT && status != FULLFORT);
+    }
+
+
+    private Optional<String> getInputFeilmelding(Forhaandsorientering forhaandsorientering) {
         if (forhaandsorientering == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "forhaandsorientering kan ikke være null");
+            return Optional.of("forhaandsorientering kan ikke være null");
         }
 
         if (forhaandsorientering.getType() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "forhaandsorientering.type kan ikke være null");
+            return Optional.of("forhaandsorientering.type kan ikke være null");
         }
 
         if (forhaandsorientering.getTekst() != null && forhaandsorientering.getTekst().isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "forhaandsorientering.tekst kan ikke være null");
+            return Optional.of("forhaandsorientering.tekst kan ikke være null");
         }
+        return Optional.empty();
     }
 }
---
+++ src/main/java/no/nav/veilarbaktivitet/service/UserInContext.java
@@ -0,0 +1,38 @@
+package no.nav.veilarbaktivitet.service;
+
+import lombok.RequiredArgsConstructor;
+import no.nav.common.auth.subject.SubjectHandler;
+import no.nav.veilarbaktivitet.domain.Person;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserInContext {
+    private final HttpServletRequest requestProvider;
+    private final BrukerService brukerService;
+
+    public Optional<Person> getUserIdent() {
+        if (BrukerService.erEksternBruker()) {
+            return SubjectHandler.getIdent().map(Person::fnr);
+        }
+
+        Optional<Person> fnr = Optional.ofNullable(requestProvider.getParameter("fnr")).map(Person::fnr);
+        Optional<Person> aktorId = Optional.ofNullable(requestProvider.getParameter("aktorId")).map(Person::aktorId);
+
+        return fnr.or(() -> aktorId);
+    }
+
+    public Optional<Person.Fnr> getFnr() {
+        return getUserIdent()
+                .flatMap(brukerService::getFNR);
+    }
+
+    public Optional<Person.AktorId> getAktorId() {
+        return getUserIdent()
+                .flatMap(brukerService::getAktorIdForPerson);
+    }
+
+}
