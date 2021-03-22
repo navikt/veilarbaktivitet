a2a65cc0e7de0548b1bde715df2936edb3257f83
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-03-22 14:03:59 +0100

    Fikse migreringsscriptet for fho lest

--- src/main/resources/db/migration/V1_39__fho_lest.sql
+++ src/main/resources/db/migration/V1_39__fho_lest.sql
@@ -1,9 +1,9 @@
 alter table ARENA_FORHAANDSORIENTERING
-    add column lest timestamp;
+    add lest timestamp;
 
 create index arena_fho_lest_idx on ARENA_FORHAANDSORIENTERING (lest, arenaaktivitet_id);
 
 alter table AKTIVITET
-    add column fho_lest timestamp;
+    add fho_lest timestamp;
 
 create index aktivitet_fho_lest_idx on AKTIVITET (AKTIVITET_ID, VERSJON, GJELDENDE, fho_lest);
