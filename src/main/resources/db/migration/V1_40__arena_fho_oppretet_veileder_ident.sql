77db71d93cf6df176ce9f41daba9b277cfb1027d
hakon planke holm <hakon.planke.holm@nav.no>
2021-04-07 11:14:57 +0200

    fikse kollone navn

--- src/main/resources/db/migration/V1_40__arena_fho_oppretet_veileder_ident.sql
+++ src/main/resources/db/migration/V1_40__arena_fho_oppretet_veileder_ident.sql
@@ -1,3 +1,3 @@
-alter table ARENA_FORHAANDSORIENTERING add oppretet_av_ident nvarchar2(255) not null default 'ikke-satt';
+alter table ARENA_FORHAANDSORIENTERING add opprettet_av_ident nvarchar2(255) not null default 'ikke-satt';
 
-alter table ARENA_FORHAANDSORIENTERING alter column oppretet_av_ident drop default;
+alter table ARENA_FORHAANDSORIENTERING alter column opprettet_av_ident drop default;
