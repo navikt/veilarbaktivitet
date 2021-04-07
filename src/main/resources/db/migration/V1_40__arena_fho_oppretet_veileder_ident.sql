96dbc542a3b1267de47e47efa5d6d0c7742b801a
hakon planke holm <hakon.planke.holm@nav.no>
2021-04-07 11:21:34 +0200

    forenkler migreringen pga forskjell mellom h2 og oracle

--- src/main/resources/db/migration/V1_40__arena_fho_oppretet_veileder_ident.sql
+++ src/main/resources/db/migration/V1_40__arena_fho_oppretet_veileder_ident.sql
@@ -1,3 +1,1 @@
-alter table ARENA_FORHAANDSORIENTERING add opprettet_av_ident nvarchar2(255) not null default 'ikke-satt';
-
-alter table ARENA_FORHAANDSORIENTERING alter column opprettet_av_ident drop default;
+alter table ARENA_FORHAANDSORIENTERING add opprettet_av_ident nvarchar2(255);
