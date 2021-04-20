a6475a6ad7e18cfa9b4f9b474a833b94537158f4
hakonph <hakon.planke.holm@nav.no>
2021-04-20 10:57:51 +0200

    slette feeds koden (#156)

--- src/main/java/no/nav/veilarbaktivitet/feed/common/FeedAuthorizationModule.java
+++
@@ -1,6 +0,0 @@
-package no.nav.veilarbaktivitet.feed.common;
-
-@FunctionalInterface
-public interface FeedAuthorizationModule {
-    boolean isRequestAuthorized(String feedname);
-}
--- src/main/java/no/nav/veilarbaktivitet/feed/common/OutInterceptor.java
+++
@@ -1,7 +0,0 @@
-package no.nav.veilarbaktivitet.feed.common;
-
-import javax.ws.rs.client.Invocation;
-
-public interface OutInterceptor {
-    void apply(Invocation.Builder builder);
-}
