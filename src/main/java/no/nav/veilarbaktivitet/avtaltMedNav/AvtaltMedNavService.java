f256eecb0696d40f81e92f802836c77c09b9a9c8
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-05-25 16:36:33 +0200

    Fix micrometer tag/label oppsett

--- src/main/java/no/nav/veilarbaktivitet/avtaltMedNav/AvtaltMedNavService.java
+++ src/main/java/no/nav/veilarbaktivitet/avtaltMedNav/AvtaltMedNavService.java
@@ -1,22 +1,17 @@
 package no.nav.veilarbaktivitet.avtaltMedNav;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Tag;
-import lombok.RequiredArgsConstructor;
 import no.nav.veilarbaktivitet.db.dao.AktivitetDAO;
 import no.nav.veilarbaktivitet.domain.AktivitetDTO;
 import no.nav.veilarbaktivitet.domain.AktivitetData;
 import no.nav.veilarbaktivitet.domain.AktivitetTransaksjonsType;
-import no.nav.veilarbaktivitet.domain.AktivitetTypeData;
 import no.nav.veilarbaktivitet.mappers.AktivitetDTOMapper;
 import no.nav.veilarbaktivitet.service.MetricService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -25,35 +20,22 @@ public class AvtaltMedNavService {
     private final MeterRegistry meterRegistry;
     private final AktivitetDAO dao;
 
+    public static final String AVTALT_MED_NAV_COUNTER = "aktivitet.avtalt.med.nav";
+    public static final String AKTIVITET_TYPE_LABEL = "AktivitetType";
+    public static final String FORHAANDSORIENTERING_TYPE_LABEL = "ForhaandsorienteringType";
+
+
+
     public AvtaltMedNavService(MetricService metricService,
              MeterRegistry meterRegistry,
             AktivitetDAO dao) {
         this.meterRegistry = meterRegistry;
         this.metricService = metricService;
         this.dao = dao;
-        List<Tag> tags = Arrays.stream(Forhaandsorientering.Type.values()).map(t -> new Tag() {
-                    @Override
-                    public String getKey() {
-                        return t.name();
-                    }
-
-                    @Override
-                    public String getValue() {
-                        return "ForhaandsorienteringType";
-                    }
-                }).collect(Collectors.toList());
-        tags.addAll(Arrays.stream(AktivitetTypeData.values()).map(t -> new Tag() {
-            @Override
-            public String getKey() {
-                return t.name();
-            }
-
-            @Override
-            public String getValue() {
-                return "AktivitetType";
-            }
-        }).collect(Collectors.toList()));
-        meterRegistry.counter("aktivitet.avtalt.med.nav", tags);
+        Counter.builder(AVTALT_MED_NAV_COUNTER)
+                .description("Antall aktiviteter som er avtalt med NAV")
+                .tags(AKTIVITET_TYPE_LABEL, "", FORHAANDSORIENTERING_TYPE_LABEL, "")
+                .register(meterRegistry);
     }
 
     AktivitetData hentAktivitet(long aktivitetId) {
@@ -79,7 +61,8 @@ public class AvtaltMedNavService {
 
         metricService.oppdaterAktivitetMetrikk(aktivitet, true, aktivitet.isAutomatiskOpprettet());
 
-        meterRegistry.counter("aktivitet.avtalt.med.nav", forhaandsorientering.getType().name(), aktivitet.getAktivitetType().name()).increment();
+
+        meterRegistry.counter(AVTALT_MED_NAV_COUNTER, FORHAANDSORIENTERING_TYPE_LABEL, forhaandsorientering.getType().name(), AKTIVITET_TYPE_LABEL, aktivitet.getAktivitetType().name()).increment();
 
 
         return AktivitetDTOMapper.mapTilAktivitetDTO(dao.hentAktivitet(aktivitetId));
