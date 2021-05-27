dbc0f78728fea4f66de9222c01532e33fbe70332
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-05-27 16:03:50 +0200

    Litt basic javadoc på domeneobjektet AktivitetData

--- src/main/java/no/nav/veilarbaktivitet/domain/AktivitetData.java
+++ src/main/java/no/nav/veilarbaktivitet/domain/AktivitetData.java
@@ -14,23 +14,54 @@ import java.util.Date;
 @ToString(of = {"id", "versjon", "aktivitetType", "status", "endretDato", "transaksjonsType", "avtalt"})
 public class AktivitetData {
 
+    /**
+     * Teknisk id for aktiviteten
+     */
     Long id;
+    /**
+     * Versjon inkrementeres når det utføres en transasjon på aktiviteten.
+     * Denne er en global sekvens for alle aktiviteter, men for en enkelt aktivitet, vil en sortering på versjon gi rekkefølgen på transaksjonene
+     */
     Long versjon;
+    /**
+     * AktørId for eksternbruker som 'eier' aktivitetsplanen
+     */
     String aktorId;
+    /**
+     * Tittel på aktiviteten
+     */
     String tittel;
+    /**
+     * Type aktivitet
+     */
     AktivitetTypeData aktivitetType;
     String beskrivelse;
+    /**
+     * Overordnet status for aktiviteten i aktivitetsplanen
+     */
     AktivitetStatus status;
     String avsluttetKommentar;
+    /**
+     * Gjelder for en AktivitetData versjon, og sier om det var NAV eller BRUKER som utførte en {@link AktivitetTransaksjonsType} på aktiviteten
+     */
     InnsenderData lagtInnAv;
     Date fraDato;
     Date tilDato;
     String lenke;
     Date opprettetDato;
     Date endretDato;
+    /**
+     * Gjelder for en AktivitetData versjon, og angir ident som utførte en {@link AktivitetTransaksjonsType} på aktiviteten
+     */
     String endretAv;
+    /**
+     * Merket som 'Avtalt med Nav'
+     */
     boolean avtalt;
     Forhaandsorientering forhaandsorientering;
+    /**
+     * Alle type endringer på aktiviteten angis med en transaksjonstype.
+     */
     AktivitetTransaksjonsType transaksjonsType;
     Date historiskDato;
     String kontorsperreEnhetId;
