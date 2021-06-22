b58cfb7d55af9c0618453219be229b0e0310526d
Thea Hove Johansen <thea.hove.johansen@nav.no>
2021-06-22 10:51:55 +0200

    Opprette OppfolgingStatusClient i forbindelse med rek.bistand

---
+++ src/main/java/no/nav/veilarbaktivitet/oppfolging_status/OppfolgingStatusDTO.java
@@ -0,0 +1,11 @@
+package no.nav.veilarbaktivitet.oppfolging_status;
+
+import lombok.Data;
+
+@Data
+public class OppfolgingStatusDTO {
+
+    private boolean underOppfolging;
+
+    private boolean erManuell;
+}
