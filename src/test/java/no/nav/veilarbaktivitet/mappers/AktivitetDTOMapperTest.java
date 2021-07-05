8d07f0aa27097ac8fdd672d782b1ca7f8e30076a
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-07-05 12:34:54 +0200

    Ikke eksponere samtalereferat for ekstern bruker hvis ikke publisert (#221)
    * Samtalereferat eksponeres for eksterne brukere ikke når ikke publisert
    
    * Fjernet norsk bokstav i metodenavn

--- src/test/java/no/nav/veilarbaktivitet/mappers/AktivitetDTOMapperTest.java
+++ src/test/java/no/nav/veilarbaktivitet/mappers/AktivitetDTOMapperTest.java
@@ -75,15 +75,15 @@ public class AktivitetDTOMapperTest {
 
     @Test
     public void skalMappeStillingSokData() {
-        AktivitetData nyttStillingssøk = AktivitetDataTestBuilder.nyttStillingssøk();
-        AktivitetDTO aktivitetDTO = AktivitetDTOMapper.mapTilAktivitetDTO(nyttStillingssøk, false);
+        AktivitetData nyttStillingssok = AktivitetDataTestBuilder.nyttStillingssøk();
+        AktivitetDTO aktivitetDTO = AktivitetDTOMapper.mapTilAktivitetDTO(nyttStillingssok, false);
         SoftAssertions.assertSoftly( s -> {
             s.assertThat(aktivitetDTO.getType()).isEqualTo(AktivitetTypeDTO.STILLING);
-            s.assertThat(aktivitetDTO.getArbeidsgiver()).isEqualTo(nyttStillingssøk.getStillingsSoekAktivitetData().getArbeidsgiver());
-            s.assertThat(aktivitetDTO.getStillingsTittel()).isEqualTo(nyttStillingssøk.getStillingsSoekAktivitetData().getStillingsTittel());
-            s.assertThat(aktivitetDTO.getArbeidssted()).isEqualTo(nyttStillingssøk.getStillingsSoekAktivitetData().getArbeidssted());
-            s.assertThat(aktivitetDTO.getKontaktperson()).isEqualTo(nyttStillingssøk.getStillingsSoekAktivitetData().getKontaktPerson());
-            s.assertThat(aktivitetDTO.getEtikett()).isEqualTo(Helpers.Etikett.getDTO(nyttStillingssøk.getStillingsSoekAktivitetData().getStillingsoekEtikett()));
+            s.assertThat(aktivitetDTO.getArbeidsgiver()).isEqualTo(nyttStillingssok.getStillingsSoekAktivitetData().getArbeidsgiver());
+            s.assertThat(aktivitetDTO.getStillingsTittel()).isEqualTo(nyttStillingssok.getStillingsSoekAktivitetData().getStillingsTittel());
+            s.assertThat(aktivitetDTO.getArbeidssted()).isEqualTo(nyttStillingssok.getStillingsSoekAktivitetData().getArbeidssted());
+            s.assertThat(aktivitetDTO.getKontaktperson()).isEqualTo(nyttStillingssok.getStillingsSoekAktivitetData().getKontaktPerson());
+            s.assertThat(aktivitetDTO.getEtikett()).isEqualTo(Helpers.Etikett.getDTO(nyttStillingssok.getStillingsSoekAktivitetData().getStillingsoekEtikett()));
 
             s.assertAll();
         });
@@ -153,6 +153,55 @@ public class AktivitetDTOMapperTest {
             s.assertThat(aktivitetDTO.getKanal()).isEqualTo(nyMoteAktivitet.getMoteData().getKanal());
             s.assertThat(aktivitetDTO.getReferat()).isEqualTo(nyMoteAktivitet.getMoteData().getReferat());
             s.assertThat(aktivitetDTO.isErReferatPublisert()).isEqualTo(nyMoteAktivitet.getMoteData().isReferatPublisert());
+            s.assertAll();
+        });
+    }
+
+    @Test
+    public void skalIkkeViseReferatNaarEksternOgIkkePublisert() {
+        AktivitetData moteAktivitet = AktivitetDataTestBuilder.nyMoteAktivitet();
+        MoteData moteData = moteAktivitet.getMoteData().withReferat("Referat").withReferatPublisert(false);
+        AktivitetData nyMoteAktivitet = moteAktivitet.withMoteData(moteData);
+
+        AktivitetDTO aktivitetDTO = AktivitetDTOMapper.mapTilAktivitetDTO(nyMoteAktivitet, true);
+        SoftAssertions.assertSoftly( s -> {
+            s.assertThat(aktivitetDTO.getReferat()).isNull();
+            s.assertThat(aktivitetDTO.isErReferatPublisert()).isEqualTo(false);
+            s.assertAll();
+        });
+    }
+
+    @Test
+    public void skalViseReferatNaarInternOgIkkePublisert() {
+        AktivitetData moteAktivitet = AktivitetDataTestBuilder.nyMoteAktivitet();
+        MoteData moteData = moteAktivitet.getMoteData().withReferat("Referat").withReferatPublisert(false);
+        AktivitetData nyMoteAktivitet = moteAktivitet.withMoteData(moteData);
+
+        AktivitetDTO aktivitetDTO = AktivitetDTOMapper.mapTilAktivitetDTO(nyMoteAktivitet, false);
+        SoftAssertions.assertSoftly( s -> {
+            s.assertThat(aktivitetDTO.getReferat()).isEqualTo("Referat");
+            s.assertThat(aktivitetDTO.isErReferatPublisert()).isEqualTo(false);
+            s.assertAll();
+        });
+    }
+
+    @Test
+    public void skalViseReferatNaarReferatErPublisert() {
+        AktivitetData moteAktivitet = AktivitetDataTestBuilder.nyMoteAktivitet();
+        MoteData moteData = moteAktivitet.getMoteData().withReferat("Referat").withReferatPublisert(true);
+        AktivitetData nyMoteAktivitet = moteAktivitet.withMoteData(moteData);
+
+        AktivitetDTO aktivitetDTO = AktivitetDTOMapper.mapTilAktivitetDTO(nyMoteAktivitet, true);
+        SoftAssertions.assertSoftly( s -> {
+            s.assertThat(aktivitetDTO.getReferat()).isEqualTo("Referat");
+            s.assertThat(aktivitetDTO.isErReferatPublisert()).isEqualTo(true);
+            s.assertAll();
+        });
+
+        AktivitetDTO aktivitetDTO2 = AktivitetDTOMapper.mapTilAktivitetDTO(nyMoteAktivitet, false);
+        SoftAssertions.assertSoftly( s -> {
+            s.assertThat(aktivitetDTO2.getReferat()).isEqualTo("Referat");
+            s.assertThat(aktivitetDTO2.isErReferatPublisert()).isEqualTo(true);
             s.assertAll();
         });
     }
