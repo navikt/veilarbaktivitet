82a28081d2bc64a7422c867ab5400fbfa8499043
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-07-06 13:56:56 +0200

    @Service

--- src/main/java/no/nav/veilarbaktivitet/kafkatest/KafkaTestConsumer.java
+++ src/main/java/no/nav/veilarbaktivitet/kafkatest/KafkaTestConsumer.java
@@ -3,7 +3,10 @@ package no.nav.veilarbaktivitet.kafkatest;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.veilarbaktivitet.avro.ForesporselOmDelingAvCv;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
 @Slf4j
+@Service
 public class KafkaTestConsumer {
 
     @KafkaListener(topics = "pto.veilarbaktivitet-test-toppic")
