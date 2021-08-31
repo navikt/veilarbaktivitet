4ae0f900a213f7b9e71588f3858e006df49901a6
haakonholm <hakon.planke.holm@nav.no>
2021-08-31 20:02:07 +0200

    fikser DelingAvCvITest

--- src/test/java/no/nav/veilarbaktivitet/util/WireMockUtil.java
+++ src/test/java/no/nav/veilarbaktivitet/util/WireMockUtil.java
@@ -1,10 +1,5 @@
 package no.nav.veilarbaktivitet.util;
 
-import com.github.tomakehurst.wiremock.matching.StringValuePattern;
-import no.nav.common.types.identer.EksternBrukerId;
-
-import java.util.Arrays;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 
@@ -20,16 +15,35 @@ public class WireMockUtil {
         boolean underOppfolging = mockBruker.isUnderOppfolging();
         boolean harBruktNivaa4 = mockBruker.isHarBruktNivaa4();
 
+        oppfolging(fnr, underOppfolging);
+        manuell(fnr, erManuell, erReservertKrr, kanVarsles);
+        kvp(aktorId, erUnderKvp);
+        aktor(fnr, aktorId);
+        nivaa4(fnr, harBruktNivaa4);
+    }
+
+    private static void oppfolging(String fnr, boolean underOppfolging) {
         stubFor(get("/veilarboppfolging/api/v2/oppfolging?fnr=" + fnr)
                 .willReturn(ok()
                         .withHeader("Content-Type", "text/json")
                         .withBody("{\"erUnderOppfolging\":" + underOppfolging + "}")));
+    }
 
+    private static void nivaa4(String fnr, boolean harBruktNivaa4) {
+        stubFor(get("/veilarbperson/api/person/" + fnr + "/harNivaa4")
+                .willReturn(ok()
+                        .withHeader("Content-Type", "text/json")
+                        .withBody("{\"harbruktnivaa4\":" + harBruktNivaa4 + "}")));
+    }
+
+    private static void manuell(String fnr, boolean erManuell, boolean erReservertKrr, boolean kanVarsles) {
         stubFor(get("/veilarboppfolging/api/v2/manuell/status?fnr=" + fnr)
                 .willReturn(ok()
                         .withHeader("Content-Type", "text/json")
                         .withBody("{\"erUnderManuellOppfolging\":" + erManuell + ",\"krrStatus\":{\"kanVarsles\":" + kanVarsles + ",\"erReservert\":" + erReservertKrr + "}}")));
+    }
 
+    private static void kvp(String aktorId, boolean erUnderKvp) {
         if (erUnderKvp) {
             stubFor(get("/veilarboppfolging/api/v2/kvp?aktorId=" + aktorId)
                     .willReturn(ok()
@@ -39,10 +53,12 @@ public class WireMockUtil {
             stubFor(get("/veilarboppfolging/api/v2/kvp?aktorId=" + aktorId)
                     .willReturn(aResponse().withStatus(204)));
         }
+    }
 
+    private static void aktor(String fnr, String aktorId) {
         stubFor(get("/aktorTjeneste/identer?gjeldende=true&identgruppe=AktoerId")
-            .withHeader("Nav-Personidenter", equalTo(fnr))
-                .willReturn(ok().withBody(
+                .withHeader("Nav-Personidenter", equalTo(fnr))
+                .willReturn(ok().withBody("" +
                         "{" +
                         "  \"" + fnr + "\": {" +
                         "    \"identer\": [" +
@@ -55,10 +71,21 @@ public class WireMockUtil {
                         "  }" +
                         "}")));
 
-        stubFor(get("/veilarbperson/api/person/" + fnr + "/harNivaa4")
-                .willReturn(ok()
-                        .withHeader("Content-Type", "text/json")
-                        .withBody("{\"harbruktnivaa4\":" + harBruktNivaa4 + "}")));
-
+        stubFor(get("/aktorTjeneste/identer?gjeldende=true&identgruppe=NorskIdent")
+                .withHeader("Nav-Personidenter", equalTo(aktorId))
+                .willReturn(ok().withBody("" +
+                        "{" +
+                        "  \"" + aktorId + "\": {" +
+                        "    \"identer\": [" +
+                        "      {" +
+                        "        \"ident\": \"" + fnr + "\"," +
+                        "        \"identgruppe\": \"NorskIdent\"," +
+                        "        \"gjeldende\": true" +
+                        "      }" +
+                        "    ]" +
+                        "  }" +
+                        "}")));
     }
+
+
 }
