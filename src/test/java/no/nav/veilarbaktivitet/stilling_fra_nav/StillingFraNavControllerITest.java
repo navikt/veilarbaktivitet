4583071a34550ef734ec54d262346afef0b14d95
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-09-03 10:23:19 +0200

    nytt testcase for StillingFraNavControllerITest

--- src/test/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavControllerITest.java
+++ src/test/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavControllerITest.java
@@ -93,7 +93,7 @@ public class StillingFraNavControllerITest {
     }
 
     @Test
-    public void happy_case() {
+    public void happy_case_svar_ja() {
         MockBruker mockBruker = MockBruker.happyBruker("1234", "4321");
         AktivitetDTO aktivitetDTO = opprettStillingFraNav(mockBruker);
         DelingAvCvDTO delingAvCvDTO = DelingAvCvDTO.builder()
@@ -156,6 +156,75 @@ public class StillingFraNavControllerITest {
             assertions.assertAll();
         });
     }
+
+    @Test
+    public void happy_case_svar_nei() {
+        MockBruker mockBruker = MockBruker.happyBruker("1234", "4321");
+        AktivitetDTO aktivitetDTO = opprettStillingFraNav(mockBruker);
+        DelingAvCvDTO delingAvCvDTO = DelingAvCvDTO.builder()
+                .aktivitetVersjon(Long.parseLong(aktivitetDTO.getVersjon()))
+                .kanDeles(false)
+                .build();
+
+        // Kafka consumer for svarmelding til rekrutteringsbistand.
+        final Consumer<String, DelingAvCvRespons> consumer = createConsumer();
+
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .param("aktivitetId", aktivitetDTO.getId())
+                .body(delingAvCvDTO)
+                .when()
+                .put("http://localhost:" + port + "/veilarbaktivitet/api/stillingFraNav/kanDeleCV?fnr=" + mockBruker.getFnr())
+                .then()
+                .assertThat().statusCode(HttpStatus.OK.value())
+                .extract().response();
+
+        AktivitetDTO actualAktivitet = response.as(AktivitetDTO.class);
+
+        CvKanDelesData expectedCvKanDelesData = CvKanDelesData.builder()
+                .kanDeles(false)
+                .endretAv(FilterTestConfig.NAV_IDENT_ITEST)
+                .endretAvType(InnsenderData.NAV)
+                // kopierer systemgenererte attributter
+                .endretTidspunkt(actualAktivitet.getStillingFraNavData().getCvKanDelesData().endretTidspunkt)
+                .build();
+
+
+        AktivitetDTO expectedAktivitet = aktivitetDTO.toBuilder()
+                .status(AktivitetStatus.AVBRUTT)
+                .avsluttetKommentar("Automatisk avsluttet fordi cv ikke skal deles")
+                .build();
+
+        expectedAktivitet.getStillingFraNavData().setCvKanDelesData(expectedCvKanDelesData);
+
+        testService.assertOppdatertAktivitet(expectedAktivitet, actualAktivitet);
+
+
+        // Sjekk at svarmelding sendt til rekrutteringsbistand
+        final ConsumerRecord<String, DelingAvCvRespons> record = getSingleRecord(consumer, utTopic, 5000);
+        DelingAvCvRespons value = record.value();
+
+        Svar expectedSvar = Svar.newBuilder()
+                .setSvar(false)
+                .setSvartAvBuilder(Ident.newBuilder()
+                        .setIdent(FilterTestConfig.NAV_IDENT_ITEST)
+                        .setIdentType(IdentTypeEnum.NAV_IDENT))
+                // kopier systemgenererte felter
+                .setSvarTidspunkt(value.getSvar().getSvarTidspunkt())
+                .build();
+
+
+        SoftAssertions.assertSoftly(assertions -> {
+            assertions.assertThat(value.getBestillingsId()).isEqualTo(aktivitetDTO.getStillingFraNavData().getBestillingsId());
+            assertions.assertThat(value.getAktorId()).isEqualTo(mockBruker.getAktorId());
+            assertions.assertThat(value.getAktivitetId()).isEqualTo(aktivitetDTO.getId());
+            assertions.assertThat(value.getTilstand()).isEqualTo(TilstandEnum.HAR_SVART);
+            assertions.assertThat(value.getSvar()).isEqualTo(expectedSvar);
+            assertions.assertAll();
+        });
+    }
+
 
     private AktivitetDTO opprettStillingFraNav(MockBruker mockBruker) {
         final Consumer<String, DelingAvCvRespons> consumer = createConsumer();
