f33f38d9ebc8f6126778d3c17903ea25f3fcdeaa
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-10-08 15:55:16 +0200

    stilling_fra_nav livslopsstatus not null

--- src/main/resources/db/migration/V1_56__stilling_fra_nav_status.sql
+++ src/main/resources/db/migration/V1_56__stilling_fra_nav_status.sql
@@ -1,2 +1,9 @@
 alter table STILLING_FRA_NAV
-    add LIVSLOPSSTATUS varchar(255);
+    add LIVSLOPSSTATUS varchar(255) not null default 'HAR_SVART';
+
+update STILLING_FRA_NAV
+set LIVSLOPSSTATUS='PROVER_VARSLING'
+where CV_KAN_DELES is null;
+
+alter table STILLING_FRA_NAV
+    ALTER COLUMN LIVSLOPSSTATUS varchar(255) not null;
