15beac6da6d060661b17314af5fd1383ead558e1
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-10-11 11:39:59 +0200

    Glemte commit etter update før ny ddl

--- src/main/resources/db/migration/V1_56__stilling_fra_nav_status.sql
+++ src/main/resources/db/migration/V1_56__stilling_fra_nav_status.sql
@@ -5,5 +5,7 @@ update STILLING_FRA_NAV
 set LIVSLOPSSTATUS='PROVER_VARSLING'
 where CV_KAN_DELES is null;
 
+commit;
+
 alter table STILLING_FRA_NAV
     ALTER COLUMN LIVSLOPSSTATUS varchar(255) default (null) not null;
