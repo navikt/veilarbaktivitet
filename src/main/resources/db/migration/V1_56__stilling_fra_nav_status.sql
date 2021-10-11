67c37b4b1a4f3cd46ca23a0223f7665e30dd1291
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2021-10-11 05:19:19 -0700

    MODIFY for å endre kolonne (#310)

--- src/main/resources/db/migration/V1_56__stilling_fra_nav_status.sql
+++ src/main/resources/db/migration/V1_56__stilling_fra_nav_status.sql
@@ -1,11 +1,9 @@
 alter table STILLING_FRA_NAV
     add LIVSLOPSSTATUS varchar(255) default 'HAR_SVART' not null;
 
+alter table STILLING_FRA_NAV
+    ALTER COLUMN LIVSLOPSSTATUS varchar(255) default (null) not null;
+
 update STILLING_FRA_NAV
 set LIVSLOPSSTATUS='PROVER_VARSLING'
 where CV_KAN_DELES is null;
-
-commit;
-
-alter table STILLING_FRA_NAV
-    ALTER COLUMN LIVSLOPSSTATUS varchar(255) default (null) not null;
