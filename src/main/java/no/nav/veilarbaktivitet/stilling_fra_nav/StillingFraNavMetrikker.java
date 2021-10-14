5951aa7d5937e10175bce8f71fd20b130516ee36
haakonholm <hakon.planke.holm@nav.no>
2021-10-14 14:59:09 +0200

    fikser phrometius oppset

--- src/main/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavMetrikker.java
+++ src/main/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavMetrikker.java
@@ -8,32 +8,44 @@ import org.springframework.stereotype.Component;
 @Component
 class StillingFraNavMetrikker {
     private final MeterRegistry meterRegistry;
+    private static final String stillingFraNavKanDeles = "stilling_fra_nav_kan_deles";
+    private static final String stillingFraNavTidsfristUtlopt = "stilling_fra_nav_tidsfrist_utlopt";
+    private static final String stillingFraNavManueltAvbrutt = "stilling_fra_nav_manuelt_avbrutt";
+    private static final String erEksternBruker = "er_ekstern_bruker";
+    private static final String kanDele = "kan_dele";
 
     StillingFraNavMetrikker(MeterRegistry meterRegistry) {
         this.meterRegistry = meterRegistry;
-        meterRegistry.counter("StillingFraNavKanDeles", "erEksternBruker", "", "kanDele", "");
-        meterRegistry.counter("StillingFraNavManueltAvbrutt", "erEksternBruker", "");
-        meterRegistry.counter("StillingFraNavTidsfristUtlopt");
+
+
+        meterRegistry.counter(stillingFraNavKanDeles, erEksternBruker, "" + true, kanDele, "" + true);
+        meterRegistry.counter(stillingFraNavKanDeles, erEksternBruker, "" + true, kanDele, "" + false);
+        meterRegistry.counter(stillingFraNavKanDeles, erEksternBruker, "" + false, kanDele, "" + true);
+        meterRegistry.counter(stillingFraNavKanDeles, erEksternBruker, "" + false, kanDele, "" + false);
+        meterRegistry.counter(stillingFraNavManueltAvbrutt, erEksternBruker, "" + true);
+        meterRegistry.counter(stillingFraNavManueltAvbrutt, erEksternBruker, "" + false);
+
+        meterRegistry.counter(stillingFraNavTidsfristUtlopt);
     }
 
 
     void countSvar(boolean erEksternBruker, boolean svar) {
-        Counter.builder("StillingFraNavKanDeles")
-                .tag("erEksternBruker", "" + erEksternBruker)
-                .tag("kanDele", "" + svar)
+        Counter.builder(stillingFraNavKanDeles)
+                .tag(StillingFraNavMetrikker.erEksternBruker, "" + erEksternBruker)
+                .tag(kanDele, "" + svar)
                 .register(meterRegistry)
                 .increment();
     }
 
     void countManuletAvbrutt(InnsenderData brukerType) {
-        Counter.builder("StillingFraNavManueltAvbrutt")
-                .tag("erEksternBruker", "" + brukerType.equals(InnsenderData.BRUKER))
+        Counter.builder(stillingFraNavManueltAvbrutt)
+                .tag(erEksternBruker, "" + brukerType.equals(InnsenderData.BRUKER))
                 .register(meterRegistry)
                 .increment();
     }
 
     void countTidsfristUtlopt() {
-        Counter.builder("StillingFraNavTidsfristUtlopt")
+        Counter.builder(stillingFraNavTidsfristUtlopt)
                 .register(meterRegistry)
                 .increment();
     }
