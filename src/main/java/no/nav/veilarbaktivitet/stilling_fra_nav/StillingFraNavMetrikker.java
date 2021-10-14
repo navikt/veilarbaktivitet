3dbeeab62e7615e2345b4fe2b8f39b600d743335
hakonph <hakon.planke.holm@nav.no>
2021-10-14 11:01:06 +0200

    metrikker avbrut utløpt stillingfranav (#319)

--- src/main/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavMetrikker.java
+++ src/main/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavMetrikker.java
@@ -3,6 +3,7 @@ package no.nav.veilarbaktivitet.stilling_fra_nav;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
+import no.nav.veilarbaktivitet.person.InnsenderData;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +12,23 @@ class StillingFraNavMetrikker {
     private final MeterRegistry meterRegistry;
 
     void countSvar(boolean erEksternBruker, boolean svar) {
-        Counter.builder("StillingFraNavKanDeles").tag("erEksternBruker", "" + erEksternBruker).tag("kanDele", "" + svar).register(meterRegistry).increment();
+        Counter.builder("StillingFraNavKanDeles")
+                .tag("erEksternBruker", "" + erEksternBruker)
+                .tag("kanDele", "" + svar)
+                .register(meterRegistry)
+                .increment();
+    }
+
+    void countManuletAvbrutt(InnsenderData brukerType) {
+        Counter.builder("StillingFraNavManueltAvbrutt")
+                .tag("erEksternBruker", "" + brukerType.equals(InnsenderData.BRUKER))
+                .register(meterRegistry)
+                .increment();
+    }
+
+    void countTidsfristUtlopt() {
+        Counter.builder("StillingFraNavTidsfristUtlopt")
+                .register(meterRegistry)
+                .increment();
     }
 }
