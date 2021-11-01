5a56bcb7dd908cbffdfccaae4ca4595f97bcb53c
haakonholm <hakon.planke.holm@nav.no>
2021-11-01 16:36:50 +0100

    migret varselstauser

--- src/main/java/no/nav/veilarbaktivitet/brukernotifikasjon/VarselStatus.java
+++ src/main/java/no/nav/veilarbaktivitet/brukernotifikasjon/VarselStatus.java
@@ -3,7 +3,6 @@ package no.nav.veilarbaktivitet.brukernotifikasjon;
 public enum VarselStatus {
     PENDING,
     SENDT,
-    FEILET,
     SKAL_AVSLUTTES,
     AVSLUTTET,
     AVBRUTT
--- src/main/resources/db/migration/V1_61__eksterntVarselStatus.sql
+++ src/main/resources/db/migration/V1_61__eksterntVarselStatus.sql
@@ -1,2 +1,2 @@
 alter table BRUKERNOTIFIKASJON
-    add varsel_kvittering_status varchar(255) not null;
+    add varsel_kvittering_status varchar(255) default ('IKKE_SATT') not null;
