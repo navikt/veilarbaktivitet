eee6247d6e76bb88d38ae1482b8bf4c5fde44748
marcjas <marc.joseph.ablian.saputil@nav.no>
2022-01-06 11:14:08 +0100

    fikse test

--- src/main/java/no/nav/veilarbaktivitet/oppfolging/oppfolginsperiode_adder/AdderDao.java
+++ src/main/java/no/nav/veilarbaktivitet/oppfolging/oppfolginsperiode_adder/AdderDao.java
@@ -2,6 +2,7 @@ package no.nav.veilarbaktivitet.oppfolging.oppfolginsperiode_adder;
 
 import lombok.RequiredArgsConstructor;
 import no.nav.veilarbaktivitet.person.Person;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,11 +20,11 @@ public class AdderDao {
                 .addValue("aktorId", aktorId.get())
                 .addValue("startDato", startDato)
                 .addValue("sluttDato", sluttDato)
-                .addValue("oppfolgingsPeriodeId", uuid);
+                .addValue("oppfolgingsperiodeId", uuid);
 
         if (sluttDato != null) {
             return template.update("""
-                UPDATE AKTIVITET SET OPPFOLGINGSPERIODE_UUID = :oppfolgingsPeriodeId
+                UPDATE AKTIVITET SET OPPFOLGINGSPERIODE_UUID = :oppfolgingsperiodeId
                 WHERE AKTOR_ID = :aktorId 
                 AND OPPRETTET_DATO BETWEEN :startDato AND :sluttDato
                 AND OPPFOLGINGSPERIODE_UUID IS NULL
@@ -31,22 +32,25 @@ public class AdderDao {
         } else {
             // aktiv (siste) periode
             return template.update("""
-                UPDATE AKTIVITET SET OPPFOLGINGSPERIODE_UUID = :oppfolgingsPeriodeId
+                UPDATE AKTIVITET SET OPPFOLGINGSPERIODE_UUID = :oppfolgingsperiodeId
                 WHERE AKTOR_ID = :aktorId 
                 AND OPPRETTET_DATO >= :startDato
                 AND OPPFOLGINGSPERIODE_UUID IS NULL
                 """, params);
         }
-
-
     }
 
     public Person.AktorId hentEnBrukerUtenOppfolgingsperiode() {
-        String aktorId = template.getJdbcTemplate().queryForObject("""
-                SELECT AKTOR_ID from AKTIVITET
-                where OPPFOLGINGSPERIODE_UUID is null
-                fetch next 1 row ONLY
-                """, String.class);
+        String aktorId;
+        try {
+            aktorId = template.getJdbcTemplate().queryForObject("""
+                    SELECT AKTOR_ID from AKTIVITET
+                    where OPPFOLGINGSPERIODE_UUID is null
+                    fetch next 1 row ONLY
+                    """, String.class);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
 
         return Person.aktorId(aktorId);
     }
