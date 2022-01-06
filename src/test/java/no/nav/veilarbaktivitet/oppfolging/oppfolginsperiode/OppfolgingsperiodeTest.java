e45d7a354f232a5d841b1fb4095940a0830780eb
marcjas <marc.joseph.ablian.saputil@nav.no>
2022-01-06 12:58:57 +0100

    test

--- src/test/java/no/nav/veilarbaktivitet/oppfolging/oppfolginsperiode/OppfolgingsperiodeTest.java
+++ src/test/java/no/nav/veilarbaktivitet/oppfolging/oppfolginsperiode/OppfolgingsperiodeTest.java
@@ -1,19 +1,48 @@
 package no.nav.veilarbaktivitet.oppfolging.oppfolginsperiode;
 
-import no.nav.veilarbaktivitet.SpringBootBaseTest;
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData;
 import no.nav.veilarbaktivitet.aktivitet.dto.AktivitetDTO;
 import no.nav.veilarbaktivitet.aktivitet.mappers.AktivitetDTOMapper;
+import no.nav.veilarbaktivitet.db.DbTestUtils;
 import no.nav.veilarbaktivitet.mock_nav_modell.MockBruker;
 import no.nav.veilarbaktivitet.mock_nav_modell.MockNavService;
 import no.nav.veilarbaktivitet.testutils.AktivitetDataTestBuilder;
+import no.nav.veilarbaktivitet.util.AktivitetTestService;
+import no.nav.veilarbaktivitet.util.KafkaTestService;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-public class OppfolgingsperiodeTest extends SpringBootBaseTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@AutoConfigureWireMock(port = 0)
+public class OppfolgingsperiodeTest {
+
+    @Autowired
+    KafkaTestService testService;
+
+    @Autowired
+    AktivitetTestService testAktivitetservice;
+
+    @Autowired
+    JdbcTemplate jdbc;
+
+    @LocalServerPort
+    int port;
+
+    @Before
+    public void setUp() {
+        DbTestUtils.cleanupTestDb(jdbc);
+    }
 
     @Autowired
     OppfolgingsperiodeCron oppfolgingsperiodeAdderCron;
