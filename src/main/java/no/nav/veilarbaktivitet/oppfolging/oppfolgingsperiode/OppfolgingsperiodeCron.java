17ba4ec613670efa5df60b3991e3ee1a3b3ebe5e
haakonph <hakon.planke.holm@nav.no>
2022-01-10 16:28:45 +0100

    bytte fra leaderelectortil schedlock

--- src/main/java/no/nav/veilarbaktivitet/oppfolging/oppfolgingsperiode/OppfolgingsperiodeCron.java
+++ src/main/java/no/nav/veilarbaktivitet/oppfolging/oppfolgingsperiode/OppfolgingsperiodeCron.java
@@ -2,8 +2,7 @@ package no.nav.veilarbaktivitet.oppfolging.oppfolgingsperiode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.javacrumbs.shedlock.core.SchedulerLock;
-import no.nav.common.job.leader_election.LeaderElectionClient;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -19,8 +18,7 @@ public class OppfolgingsperiodeCron {
             initialDelayString = "${app.env.scheduled.default.initialDelay}",
             fixedDelayString = "${app.env.scheduled.oppfolgingsperiode.fixedDelay}"
     )
-    @SchedulerLock(name = "addOppfolgingsperioder_scheduledTask",
-            lockAtLeastForString = "PT1S", lockAtMostForString = "PT14M")
+    @SchedulerLock(name = "addOppfolgingsperioder_scheduledTask", lockAtLeastFor = "PT1S", lockAtMostFor = "PT10M")
     public void addOppfolgingsperioder() {
         long antall = oppfolgingsperiodeServiceAdder.oppdater500brukere();
         log.info("oppdatert {} brukere med oppfolginsperiode", antall);
