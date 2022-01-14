bb52b2dca2f8bd953b559860da04421078cfc80e
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-01-14 05:25:57 -0800

    midlertidig stopp kafka listener (#395)
    * fix kafka aiven deserializer config
    
    * Midlertidig stopp listener

--- src/main/java/no/nav/veilarbaktivitet/oppfolging/siste_periode/SisteOppfolgingsperiodeConsumer.java
+++ src/main/java/no/nav/veilarbaktivitet/oppfolging/siste_periode/SisteOppfolgingsperiodeConsumer.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 class SisteOppfolgingsperiodeConsumer {
     private final SistePeriodeDAO sistePeriodeDAO;
 
-    @KafkaListener(topics = "${topic.inn.sisteOppfolgingsperiode}", containerFactory = "stringStringKafkaListenerContainerFactory")
+//@KafkaListener(topics = "${topic.inn.sisteOppfolgingsperiode}", containerFactory = "stringStringKafkaListenerContainerFactory")
     void opprettEllerOppdaterSistePeriode(ConsumerRecord<String, String> consumerRecord) {
         SisteOppfolgingsperiodeV1 sisteOppfolgingsperiodeV1 = JsonUtils.fromJson(consumerRecord.value(), SisteOppfolgingsperiodeV1.class);
         log.info("Siste oppfølgingsperiode: {}", sisteOppfolgingsperiodeV1);
