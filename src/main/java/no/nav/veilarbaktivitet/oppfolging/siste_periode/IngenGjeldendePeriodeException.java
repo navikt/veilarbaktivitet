6db26e514f5e65e7a44b65123196d003623770a0
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-01-20 11:01:50 +0100

    endre exception

--- src/main/java/no/nav/veilarbaktivitet/oppfolging/siste_periode/IngenGjeldendePeriodeException.java
+++ src/main/java/no/nav/veilarbaktivitet/oppfolging/siste_periode/IngenGjeldendePeriodeException.java
@@ -1,6 +1,6 @@
 package no.nav.veilarbaktivitet.oppfolging.siste_periode;
 
-public class IngenGjeldendePeriodeException extends RuntimeException {
+public class IngenGjeldendePeriodeException extends IllegalStateException {
     public IngenGjeldendePeriodeException(String msg) {
         super(msg);
     }
