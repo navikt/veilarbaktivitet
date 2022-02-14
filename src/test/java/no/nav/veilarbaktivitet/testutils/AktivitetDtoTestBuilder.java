3b3ab4d84ecf0649932baa54e38a76a92e1e46b9
haakonph <hakon.planke.holm@nav.no>
2022-02-14 15:27:58 +0100

    aktivitet dto testbuilder

---
+++ src/test/java/no/nav/veilarbaktivitet/testutils/AktivitetDtoTestBuilder.java
@@ -0,0 +1,16 @@
+package no.nav.veilarbaktivitet.testutils;
+
+import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData;
+import no.nav.veilarbaktivitet.aktivitet.dto.AktivitetDTO;
+import no.nav.veilarbaktivitet.aktivitet.dto.AktivitetTypeDTO;
+import no.nav.veilarbaktivitet.aktivitet.mappers.AktivitetDTOMapper;
+import no.nav.veilarbaktivitet.aktivitet.mappers.Helpers;
+
+public class AktivitetDtoTestBuilder {
+
+    public static AktivitetDTO nyAktivitet(AktivitetTypeDTO aktivitetTypeDTO) {
+        //TODO implementer denne ordenltig
+        AktivitetData aktivitetData = AktivitetDataTestBuilder.nyAktivitet(Helpers.Type.getData(aktivitetTypeDTO));
+        return  AktivitetDTOMapper.mapTilAktivitetDTO(aktivitetData, false);
+    }
+}
