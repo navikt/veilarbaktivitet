b13610023e8febd2bfa429616fc52546445af9cc
haakonph <hakon.planke.holm@nav.no>
2022-02-21 16:38:47 +0100

    fiks migerringskript

--- src/main/resources/db/migration/V1_73__oppdatere_oppfolingsperiode2.sql
+++ src/main/resources/db/migration/V1_73__oppdatere_oppfolingsperiode2.sql
@@ -1,2 +1,2 @@
-alter table aktivitetJobb add column maks_id number(19);
+alter table aktivitetJobb add maks_id number(19);
 update AKTIVITETJOBB set AKTIVITETJOBB.maks_id = (select max(AKTIVITET_ID) from aktivitet);
