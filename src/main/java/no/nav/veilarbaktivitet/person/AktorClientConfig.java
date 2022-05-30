8bc90361897693004f1b0e953de828e9f54c5386
stianStensli <stian.horn.stensli@nav.no>
2022-05-30 16:16:24 +0200

    bruk av AktorOppslagClient

--- src/main/java/no/nav/veilarbaktivitet/person/AktorClientConfig.java
+++ src/main/java/no/nav/veilarbaktivitet/person/AktorClientConfig.java
@@ -3,22 +3,30 @@ package no.nav.veilarbaktivitet.person;
 import no.nav.common.client.aktoroppslag.AktorOppslagClient;
 import no.nav.common.client.aktoroppslag.AktorregisterHttpClient;
 import no.nav.common.client.aktoroppslag.CachedAktorOppslagClient;
+import no.nav.common.client.aktoroppslag.PdlAktorOppslagClient;
 import no.nav.common.client.aktorregister.AktorregisterClient;
 import no.nav.common.sts.SystemUserTokenProvider;
 import no.nav.veilarbaktivitet.config.EnvironmentProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static no.nav.common.utils.EnvironmentUtils.isProduction;
+import static no.nav.common.utils.UrlUtils.createDevInternalIngressUrl;
+import static no.nav.common.utils.UrlUtils.createProdInternalIngressUrl;
 import static no.nav.veilarbaktivitet.config.ApplicationContext.APPLICATION_NAME;
 
 @Configuration
 public class AktorClientConfig {
     @Bean
-    public AktorOppslagClient aktorOppslagClient(EnvironmentProperties properties, SystemUserTokenProvider tokenProvider) {
-        AktorregisterClient aktorregisterClient = new AktorregisterHttpClient(
-                properties.getAktorregisterUrl(), APPLICATION_NAME, tokenProvider::getSystemUserToken
+    public AktorOppslagClient aktorClient(SystemUserTokenProvider systemUserTokenProvider) {
+        AktorOppslagClient aktorOppslagClient = new PdlAktorOppslagClient(
+                isProduction().orElse(false)
+                        ? createProdInternalIngressUrl("pdl-api")
+                        : createDevInternalIngressUrl("pdl-api"),
+                systemUserTokenProvider::getSystemUserToken,
+                systemUserTokenProvider::getSystemUserToken
         );
 
-        return new CachedAktorOppslagClient(aktorregisterClient);
+        return new CachedAktorOppslagClient(aktorOppslagClient);
     }
 }
