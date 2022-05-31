2b6a5517fd0b0787bfb2a9be68f6c2c3df0e3680
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2022-05-31 11:20:02 +0200

    Fiks okhttp versjon og integrasjonstester med pdl klient

--- src/main/java/no/nav/veilarbaktivitet/person/AktorClientConfig.java
+++ src/main/java/no/nav/veilarbaktivitet/person/AktorClientConfig.java
@@ -1,28 +1,33 @@
 package no.nav.veilarbaktivitet.person;
 
 import no.nav.common.client.aktoroppslag.AktorOppslagClient;
-import no.nav.common.client.aktoroppslag.AktorregisterHttpClient;
 import no.nav.common.client.aktoroppslag.CachedAktorOppslagClient;
 import no.nav.common.client.aktoroppslag.PdlAktorOppslagClient;
-import no.nav.common.client.aktorregister.AktorregisterClient;
 import no.nav.common.sts.SystemUserTokenProvider;
-import no.nav.veilarbaktivitet.config.EnvironmentProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import static no.nav.common.utils.EnvironmentUtils.isProduction;
 import static no.nav.common.utils.UrlUtils.createDevInternalIngressUrl;
 import static no.nav.common.utils.UrlUtils.createProdInternalIngressUrl;
-import static no.nav.veilarbaktivitet.config.ApplicationContext.APPLICATION_NAME;
 
 @Configuration
 public class AktorClientConfig {
+
+
     @Bean
-    public AktorOppslagClient aktorClient(SystemUserTokenProvider systemUserTokenProvider) {
+    @Profile("!dev")
+    public String pdlUrl() {
+        return isProduction().orElse(false)
+                ? createProdInternalIngressUrl("pdl-api")
+                : createDevInternalIngressUrl("pdl-api");
+    }
+
+    @Bean
+    public AktorOppslagClient aktorClient(String pdlUrl, SystemUserTokenProvider systemUserTokenProvider) {
         AktorOppslagClient aktorOppslagClient = new PdlAktorOppslagClient(
-                isProduction().orElse(false)
-                        ? createProdInternalIngressUrl("pdl-api")
-                        : createDevInternalIngressUrl("pdl-api"),
+                pdlUrl,
                 systemUserTokenProvider::getSystemUserToken,
                 systemUserTokenProvider::getSystemUserToken
         );
