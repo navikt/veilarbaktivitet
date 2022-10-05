aad3ace0ce42e9b8216a1fed72bbb17b3729893b
Lars Hartvigsen <lars.hartvigsen@nav.no>
2022-10-05 14:49:08 +0200

    Fjerner unødvendige endringer

--- src/main/java/no/nav/veilarbaktivitet/aktivitet/domain/BehandlingAktivitetData.java
+++ src/main/java/no/nav/veilarbaktivitet/aktivitet/domain/BehandlingAktivitetData.java
@@ -1,14 +1,12 @@
 package no.nav.veilarbaktivitet.aktivitet.domain;
 
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.With;
 
 @With
 @Value
 @Builder
-@EqualsAndHashCode
 public class BehandlingAktivitetData {
     String behandlingType;
     String behandlingSted;
