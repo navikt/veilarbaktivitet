ed02d27a3543c54fd233fabc94bae21860fb33c7
Marc <Marc.Joseph.Ablian.Saputil@nav.no>
2023-01-06 09:34:25 +0100

    fiks securelogs filter (#560)

---
+++ src/main/java/no/nav/veilarbaktivitet/config/filter/EnhanceSecureLogsFilter.java
@@ -0,0 +1,40 @@
+package no.nav.veilarbaktivitet.config.filter;
+
+import lombok.RequiredArgsConstructor;
+import no.nav.veilarbaktivitet.person.AuthService;
+import no.nav.veilarbaktivitet.person.Person;
+import no.nav.veilarbaktivitet.person.UserInContext;
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Profile("!dev")
+@Service
+@RequiredArgsConstructor
+public class EnhanceSecureLogsFilter implements Filter {
+
+    private final AuthService authService;
+
+    private final UserInContext userInContext;
+
+    public static final String SECURELOGS_ER_INTERN_BRUKER = "SecureLogsFilter.erInternBruker";
+    public static final String SECURELOGS_INNLOGGET_BRUKER_IDENT = "SecureLogsFilter.innloggetBrukerIdent";
+    public static final String SECURELOGS_USER_CONTEXT = "SecureLogsFilter.userContext";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String erInternBruker = Boolean.toString(authService.erInternBruker());
+        String innloggetBrukerIdent = authService.getInnloggetBrukerIdent().orElse(null);
+        String userContext = userInContext.getFnr().map(Person::get).orElse(null);
+
+        MDC.put(SECURELOGS_ER_INTERN_BRUKER, erInternBruker);
+        MDC.put(SECURELOGS_INNLOGGET_BRUKER_IDENT, innloggetBrukerIdent);
+        MDC.put(SECURELOGS_USER_CONTEXT, userContext);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
