11551bee30c33539c7ad2ff3e4ff23e35eb00908
marcjas <marc.joseph.ablian.saputil@nav.no>
2023-02-06 13:43:05 +0100

    lagre begrunnelse når vi kasserer eksternaktivitet

--- src/main/java/no/nav/veilarbaktivitet/aktivitetskort/service/KasseringsService.java
+++ src/main/java/no/nav/veilarbaktivitet/aktivitetskort/service/KasseringsService.java
@@ -4,6 +4,7 @@ package no.nav.veilarbaktivitet.aktivitetskort.service;
 import lombok.RequiredArgsConstructor;
 import no.nav.veilarbaktivitet.aktivitet.AktivitetDAO;
 import no.nav.veilarbaktivitet.aktivitet.AktivitetService;
+import no.nav.veilarbaktivitet.aktivitet.KasseringDAO;
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData;
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetStatus;
 import no.nav.veilarbaktivitet.aktivitetskort.dto.kassering.KasseringsBestilling;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 public class KasseringsService {
     private final AktivitetDAO aktivitetDAO;
     private final AktivitetService aktivitetService;
+    private final KasseringDAO kasseringDAO;
 
 
     public void kassertAktivitet(KasseringsBestilling kasseringsBestilling) throws AktivitetIkkeFunnetFeil {
@@ -24,11 +26,11 @@ public class KasseringsService {
         if (aktivitet.isEmpty()) throw new AktivitetIkkeFunnetFeil(new ErrorMessage("Kan ikke kassere aktivitet som ikke finnes"), null);
         else {
             var navIdent = Person.navIdent(kasseringsBestilling.getNavIdent().get());
-            kasserAktivitetMedTekniskId(aktivitet.get(), navIdent);
+            kasserAktivitetMedTekniskId(aktivitet.get(), navIdent, kasseringsBestilling.getBegrunnelse());
         }
     }
 
-    private void kasserAktivitetMedTekniskId(AktivitetData aktivitet, Person.NavIdent navIdent) {
+    private void kasserAktivitetMedTekniskId(AktivitetData aktivitet, Person.NavIdent navIdent, String begrunnelse) {
         if (aktivitet.getStatus() != AktivitetStatus.AVBRUTT && aktivitet.getStatus() != AktivitetStatus.FULLFORT) {
             aktivitetService.oppdaterStatus(
                     aktivitet,
@@ -36,6 +38,6 @@ public class KasseringsService {
                     navIdent.tilIdent()
             );
         }
-        aktivitetDAO.kasserAktivitet(aktivitet.getId());
+        kasseringDAO.kasserAktivitetMedBegrunnelse(aktivitet.getId(), begrunnelse);
     }
 }
