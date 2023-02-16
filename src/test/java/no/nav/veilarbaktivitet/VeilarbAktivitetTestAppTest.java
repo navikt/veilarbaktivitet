ee3ef9d07efcc72cccb94122b7b26bd2253602c6
haakonph <hakon.planke.holm@nav.no>
2023-02-16 08:56:51 +0100

    Systembruker i db

--- src/test/java/no/nav/veilarbaktivitet/VeilarbAktivitetTestAppTest.java
+++ src/test/java/no/nav/veilarbaktivitet/VeilarbAktivitetTestAppTest.java
@@ -9,6 +9,7 @@ public class VeilarbAktivitetTestAppTest {
     @Test
     void smokeTest()  {
         LocalH2Database.setUseInnMemmory();
+
         System.setProperty("server.port", "0");
 
         Assertions.assertThatCode(() ->VeilarbAktivitetTestApp.main(new String[0])).doesNotThrowAnyException();
