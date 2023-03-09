c6118105ab93854ed45f475a066d5cd608b5fb79
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-03-09 09:48:16 +0100

    fjerne_junit_4 (#635)
    * Fjerne junit 4
    
    * fjerne public fra test-metode
    
    * Litt mer cleanup

--- src/test/java/no/nav/veilarbaktivitet/nivaa4/Nivaa4ClientTest.java
+++ src/test/java/no/nav/veilarbaktivitet/nivaa4/Nivaa4ClientTest.java
@@ -17,7 +17,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 
