3846ce493a21a0be917784482990b144d45dabd5
Marc <Marc.Joseph.Ablian.Saputil@nav.no>
2023-08-21 14:13:52 +0200

    fix unleash environment config (#693)

--- src/main/kotlin/no/nav/veilarbaktivitet/unleash/UnleashConfig.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/unleash/UnleashConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.veilarbaktivitet.unleash
 
 import io.getunleash.util.UnleashConfig
+import no.nav.common.utils.EnvironmentUtils
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "unleash")
@@ -9,9 +10,10 @@ data class UnleashConfig(
     val url: String,
     val token: String,
     val instanceId: String,
-    val environment: String,
 ) {
     fun toUnleashConfig(): UnleashConfig {
+        val environment = if (EnvironmentUtils.isProduction().orElse(false)) "production" else "development"
+
         return UnleashConfig.builder()
             .appName(appName)
             .instanceId(instanceId)
