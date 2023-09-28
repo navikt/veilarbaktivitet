8dfff1d8b91193815b5bf9d460d90c6411fd8d86
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-09-28 10:25:02 +0200

    Fjern Sentient.ACTION

--- src/main/java/no/nav/veilarbaktivitet/aktivitetskort/dto/aktivitetskort/Sentiment.java
+++ src/main/java/no/nav/veilarbaktivitet/aktivitetskort/dto/aktivitetskort/Sentiment.java
@@ -6,7 +6,6 @@ package no.nav.veilarbaktivitet.aktivitetskort.dto.aktivitetskort;
 public enum Sentiment {
     POSITIVE, // Fått tilbud, Klar for oppstart, Påbegynt, Påmeldt? (gode nyheter for brukeren)
     NEGATIVE, // Ikke fått jobben, Avbrutt, Annulert, Fått avslag (dårlige nyheter for brukeren)
-    ACTION, // Venter svar fra deg (Venter handling fra brukeren)
     NEUTRAL, // Deltar, Vurderes?
     WAITING, // Mangler godkjenning, Venteliste, Vurderes? (Venter handling fra andre enn brukeren, venter på dato for oppstart)
 }
