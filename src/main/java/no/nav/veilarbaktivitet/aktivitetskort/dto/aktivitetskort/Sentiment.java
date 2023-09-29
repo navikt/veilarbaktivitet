7f70f3f2d7f8bc4323f6669a6dd9c43eb550c443
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2023-09-29 13:06:31 +0200

    Fjerne WAITING fra Sentiment

--- src/main/java/no/nav/veilarbaktivitet/aktivitetskort/dto/aktivitetskort/Sentiment.java
+++ src/main/java/no/nav/veilarbaktivitet/aktivitetskort/dto/aktivitetskort/Sentiment.java
@@ -7,5 +7,4 @@ public enum Sentiment {
     POSITIVE, // Fått tilbud, Klar for oppstart, Påbegynt, Påmeldt? (gode nyheter for brukeren)
     NEGATIVE, // Ikke fått jobben, Avbrutt, Annulert, Fått avslag (dårlige nyheter for brukeren)
     NEUTRAL, // Deltar, Vurderes?
-    WAITING, // Mangler godkjenning, Venteliste, Vurderes? (Venter handling fra andre enn brukeren, venter på dato for oppstart)
 }
