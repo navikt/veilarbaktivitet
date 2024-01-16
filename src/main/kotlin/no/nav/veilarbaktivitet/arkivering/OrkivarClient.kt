f575a05cb3ebf69bf97da14ccb995ab716079470
sigurdgroneng <sigurd.groneng@nav.no>
2024-01-16 16:21:57 +0100

    Set accept header in call to orkivar

--- src/main/kotlin/no/nav/veilarbaktivitet/arkivering/OrkivarClient.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/arkivering/OrkivarClient.kt
@@ -20,6 +20,7 @@ class OrkivarClient(private val orkivarHttpClient: OkHttpClient) {
         val payload = JsonUtils.toJson(ArkivPayload(metadata = Metadata(navn, fnr.get())))
             .toRequestBody("application/json".toMediaTypeOrNull())
         val request: Request = Request.Builder()
+            .addHeader("Accept", "application/json")
             .post(payload)
             .url(uri)
             .build()
