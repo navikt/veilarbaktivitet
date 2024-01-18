834487c16aadd959bc58a23222758bbad3c05b96
johannetronstad <johanne.tronstad@nav.no>
2024-01-18 12:23:03 +0100

    legg til logger av requestbody i hentnavn

--- src/main/kotlin/no/nav/veilarbaktivitet/person/EksternNavnService.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/person/EksternNavnService.kt
@@ -4,14 +4,18 @@ import no.nav.common.client.pdl.PdlClient
 import no.nav.common.client.utils.graphql.GraphqlRequestBuilder
 import no.nav.common.client.utils.graphql.GraphqlResponse
 import no.nav.common.client.utils.graphql.GraphqlUtils
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class EksternNavnService(val pdlClient: PdlClient) {
 
+    val logger = LoggerFactory.getLogger(this::class.java)
+
     fun hentNavn(fnr: Person.Fnr): NavnResponse {
         val graphqlRequest = GraphqlRequestBuilder<QueryVariables>("graphql/pdl/navnQuery.graphql")
             .buildRequest(QueryVariables(ident = fnr.get(), historikk = false))
+        logger.info("requestbody: ${graphqlRequest.query}" )
         return pdlClient.request(graphqlRequest, NavnResponse::class.java)
             .also { GraphqlUtils.logWarningIfError(it) }
     }
