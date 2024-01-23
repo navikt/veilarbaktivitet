127d9ae4efb888af8dd4fa8fd7475c73529bb3b8
Mads Lee Giil <mads.lee.giil@nav.no>
2024-01-23 12:02:18 +0100

    Test møte og samtalereferat

--- src/test/kotlin/no/nav/veilarbaktivitet/arkivering/mapper/AktivitetDtoArkivPayloadTest.kt
+++ src/test/kotlin/no/nav/veilarbaktivitet/arkivering/mapper/AktivitetDtoArkivPayloadTest.kt
@@ -7,25 +7,29 @@ import org.junit.jupiter.api.Test
 class AktivitetDtoArkivPayloadTest {
 
     @Test
-    fun `møte og samtalereferat skal ikke ha beskrivelse 2 ganger`() {
+    fun `Møte har riktige felt`() {
         val mote = AktivitetDataTestBuilder.nyMoteAktivitet().toArkivPayload()
         assertThat(mote.detaljer.map { it.tittel })
-            .containsExactly("Beskrivelse","Møteform",
-                "Er publisert",
+            .containsExactly(
+                "Dato",
+                "Klokkeslett",
+                "Møteform",
+                "Varighet",
                 "Møtested eller annen praktisk informasjon",
                 "Hensikt med møtet",
-                "Dato",
                 "Forberedelser til møtet",
-                "Lenke")
+            )
+    }
 
-//        assertThat(mote.detaljer.map { it.tittel }).doesNotContain("Beskrivelse")
-//        assertThat(mote.detaljer.filter { it.tittel == "Hensikt med møtet" }).isNotEmpty()
-//        val samtale = AktivitetDataTestBuilder.nySamtaleReferat().toArkivPayload()
-//        assertThat(samtale.detaljer.filter { it.tittel == "Beskrivelse" }).isEmpty()
-//        assertThat(samtale.detaljer.filter { it.tittel == "Hensikt med møtet" }).isNotEmpty()
-//        val egen = AktivitetDataTestBuilder.nyEgenaktivitet().toArkivPayload()
-//        assertThat(egen.detaljer.filter { it.tittel == "Beskrivelse" }).isNotEmpty()
-//        assertThat(egen.detaljer.filter { it.tittel == "Hensikt med møtet" }).isEmpty()
+    @Test
+    fun `Samtalereferat har riktige felt`() {
+        val mote = AktivitetDataTestBuilder.nyMoteAktivitet().toArkivPayload()
+        assertThat(mote.detaljer.map { it.tittel })
+            .containsExactly(
+                "Dato",
+                "Møteform",
+                "Samtalereferat",
+            )
     }
 
     @Test
