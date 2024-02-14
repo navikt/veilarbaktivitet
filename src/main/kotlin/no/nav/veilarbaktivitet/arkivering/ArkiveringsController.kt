16c1720dc8e801460b711b148ed3d86ec274ce20
johannetronstad <johanne.tronstad@nav.no>
2024-02-14 13:32:59 +0100

    lag funksjon for sjekk av oppdatert tidspunkt

--- src/main/kotlin/no/nav/veilarbaktivitet/arkivering/ArkiveringsController.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/arkivering/ArkiveringsController.kt
@@ -8,11 +8,11 @@ import no.nav.veilarbaktivitet.arkivering.mapper.tilMelding
 import no.nav.veilarbaktivitet.arkivering.mapper.toArkivPayload
 import no.nav.veilarbaktivitet.person.EksternNavnService
 import no.nav.veilarbaktivitet.person.UserInContext
+import no.nav.veilarbaktivitet.util.DateUtils
+import org.apache.coyote.Response
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import java.time.ZonedDateTime
 import java.util.*
@@ -27,9 +27,9 @@ class ArkiveringsController(
     private val navnService: EksternNavnService,
     private val appService: AktivitetAppService
 ) {
-    @PostMapping("/forhaandsvisning")
-    @AuthorizeFnr(auditlogMessage = "arkivere aktivitetsplan og dialog")
-    fun arkiverAktivitetsplanOgDialog(@RequestParam("oppfolgingsperiodeId") oppfølgingsperiodeId: UUID): Forhaandsvisning {
+    @GetMapping("/forhaandsvisning")
+    @AuthorizeFnr(auditlogMessage = "lag forhåndsvisning av aktivitetsplan og dialog")
+    fun forhaandsvisAktivitetsplanOgDialog(@RequestParam("oppfolgingsperiodeId") oppfølgingsperiodeId: UUID): ForhaandsvisningOutboundDTO {
         val dataHentet = ZonedDateTime.now()
         val fnr = userInContext.fnr.get()
         val result = navnService.hentNavn(fnr)
@@ -42,17 +42,43 @@ class ArkiveringsController(
         val (aktiviteterPayload, dialogerPayload) = lagDataTilOrkivar(oppfølgingsperiodeId, aktiviteter, dialoger)
         val forhaandsvisningResultat = orkivarClient.hentPdfForForhaandsvisning(fnr, navn, aktiviteterPayload, dialogerPayload)
 
-        return Forhaandsvisning(
+        return ForhaandsvisningOutboundDTO(
             forhaandsvisningResultat.uuid,
             forhaandsvisningResultat.pdf,
             dataHentet
         )
     }
 
-    data class Forhaandsvisning(
+    @PostMapping("/journalfor")
+    @AuthorizeFnr(auditlogMessage = "journalføre aktivitetsplan og dialog")
+    fun arkiverAktivitetsplanOgDialog(@RequestParam("oppfolgingsperiodeId") oppfølgingsperiodeId: UUID, @RequestBody arkiverInboundDTO: ArkiverInboundDTO) {
+        val fnr = userInContext.fnr.get()
+
+        val aktiviteter = appService.hentAktiviteterForIdentMedTilgangskontroll(fnr)
+        val dialoger = dialogClient.hentDialoger(fnr)
+
+        val oppdatertEtterForhaandsvisning = aktiviteterOgDialogerOppdatertEtter(arkiverInboundDTO.forhaandsvisningOpprettet, oppfølgingsperiodeId, aktiviteter, dialoger)
+
+        if(oppdatertEtterForhaandsvisning) throw ResponseStatusException(HttpStatus.CONFLICT)
+
+        val result = navnService.hentNavn(fnr)
+        if(result.errors?.isNotEmpty() == true) { throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR) }
+        val navn = result.data.hentPerson.navn.first().tilFornavnMellomnavnEtternavn()
+
+        val (aktiviteterPayload, dialogerPayload) = lagDataTilOrkivar(oppfølgingsperiodeId, aktiviteter, dialoger)
+        // TODO: send til kall på orkivarclient
+    }
+
+
+    data class ForhaandsvisningOutboundDTO(
         val uuid: UUID,
         val pdf: ByteArray,
         val dataHentet: ZonedDateTime
+    )
+
+    data class ArkiverInboundDTO(
+        val uuid: UUID,
+        val forhaandsvisningOpprettet: ZonedDateTime
     )
 }
 
@@ -74,4 +100,12 @@ fun lagDataTilOrkivar(oppfølgingsperiodeId: UUID, aktiviteter: List<AktivitetDa
 
     val meldingerUtenAktivitet = aktivitetDialoger[null] ?: emptyList()
     return Pair(aktiviteterPayload, meldingerUtenAktivitet.map { it.tilDialogTråd() })
+}
+
+fun aktiviteterOgDialogerOppdatertEtter(tidspunkt: ZonedDateTime, oppfølgingsperiodeId: UUID, aktiviteter: List<AktivitetData>, dialoger: List<DialogClient.DialogTråd>): Boolean {
+    val aktiviteterTidspunkt = aktiviteter.filter { it.oppfolgingsperiodeId == oppfølgingsperiodeId }.map { DateUtils.dateToZonedDateTime(it.endretDato) }
+    val dialogerTidspunkt = dialoger.filter { it.oppfolgingsperiode == oppfølgingsperiodeId }.map { it.meldinger }.flatten().map { it.sendt }
+    val sistOppdatert = (aktiviteterTidspunkt + dialogerTidspunkt).max()
+    return sistOppdatert > tidspunkt
+
 }
