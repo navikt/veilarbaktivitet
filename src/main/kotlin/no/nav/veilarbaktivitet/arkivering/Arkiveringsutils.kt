16f89bd4fd5450c0692f409250510dab501b2fb9
Mads Lee Giil <mads.lee.giil@nav.no>
2024-02-14 15:44:29 +0100

    Test logikk av endepunkt i isolasdjon

---
+++ src/main/kotlin/no/nav/veilarbaktivitet/arkivering/Arkiveringsutils.kt
@@ -0,0 +1,34 @@
+package no.nav.veilarbaktivitet.arkivering
+
+import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData
+import no.nav.veilarbaktivitet.arkivering.mapper.tilDialogTråd
+import no.nav.veilarbaktivitet.arkivering.mapper.tilMelding
+import no.nav.veilarbaktivitet.arkivering.mapper.toArkivPayload
+import no.nav.veilarbaktivitet.util.DateUtils
+import java.time.ZonedDateTime
+import java.util.*
+
+fun lagDataTilOrkivar(oppfølgingsperiodeId: UUID, aktiviteter: List<AktivitetData>, dialoger: List<DialogClient.DialogTråd>): Pair<List<ArkivAktivitet>, List<ArkivDialogtråd>> {
+    val aktivitetDialoger = dialoger.groupBy { it.aktivitetId }
+
+    val aktiviteterPayload = aktiviteter
+        .map { it ->
+            val meldingerTilhørendeAktiviteten = aktivitetDialoger[it.id.toString()]?.map {
+                it.meldinger.map { it.tilMelding() }
+            }?.flatten() ?: emptyList()
+
+            it.toArkivPayload(
+                meldinger = meldingerTilhørendeAktiviteten
+            )
+        }
+
+    val meldingerUtenAktivitet = aktivitetDialoger[null] ?: emptyList()
+    return Pair(aktiviteterPayload, meldingerUtenAktivitet.map { it.tilDialogTråd() })
+}
+
+fun aktiviteterOgDialogerOppdatertEtter(tidspunkt: ZonedDateTime, aktiviteter: List<AktivitetData>, dialoger: List<DialogClient.DialogTråd>): Boolean {
+    val aktiviteterTidspunkt = aktiviteter.map { DateUtils.dateToZonedDateTime(it.endretDato) }
+    val dialogerTidspunkt = dialoger.map { it.meldinger }.flatten().map { it.sendt }
+    val sistOppdatert = (aktiviteterTidspunkt + dialogerTidspunkt).max()
+    return sistOppdatert > tidspunkt
+}
