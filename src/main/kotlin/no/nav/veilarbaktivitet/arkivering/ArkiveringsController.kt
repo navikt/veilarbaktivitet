68847414b5f06c41dd64355d16b0de1743cfb253
Sigurd Grøneng <sigurd.groneng@nav.no>
2024-02-16 10:03:14 +0100

    Legg til støtte for forhåndsvisning av PDF til journalføring
    * Returner resultat fra Orkivar som inneholder PDF til forhåndsvisning
    
    * test forhåndsvisning
    
    * trekke ut kode som skal gjenbrukes i arkiveringscontroller
    
    * Legg til tidspunkt på respons av forhåndsvisning
    
    * lag funksjon for sjekk av oppdatert tidspunkt
    
    * Implementer metode for å arkivere
    
    * korriger endepunkt i test
    
    * Test logikk av endepunkt i isolasdjon
    
    * wip: test arkiv av rktig oppfølgingsperiode
    
    * Samle logikk for å lage payload til orkivar
    
    * Legg til flere test caser
    
    * Enkel renaming
    
    * Døp om navn på tidspunkt for henting av data og fjern unødvendig felt
    
    * fjern uuid fra forhåndsvisning
    
    * Rename funksjon
    
    ---------
    
    Co-authored-by: Mads Lee Giil <mads.lee.giil@nav.no>
    Co-authored-by: johannetronstad <johanne.tronstad@nav.no>

--- src/main/kotlin/no/nav/veilarbaktivitet/arkivering/ArkiveringsController.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/arkivering/ArkiveringsController.kt
@@ -2,17 +2,16 @@ package no.nav.veilarbaktivitet.arkivering
 
 import no.nav.poao.dab.spring_a2_annotations.auth.AuthorizeFnr
 import no.nav.veilarbaktivitet.aktivitet.AktivitetAppService
-import no.nav.veilarbaktivitet.arkivering.mapper.tilDialogTråd
-import no.nav.veilarbaktivitet.arkivering.mapper.tilMelding
-import no.nav.veilarbaktivitet.arkivering.mapper.toArkivPayload
+import no.nav.veilarbaktivitet.arkivering.Arkiveringslogikk.aktiviteterOgDialogerOppdatertEtter
+import no.nav.veilarbaktivitet.arkivering.Arkiveringslogikk.lagArkivPayload
 import no.nav.veilarbaktivitet.person.EksternNavnService
+import no.nav.veilarbaktivitet.person.Navn
+import no.nav.veilarbaktivitet.person.Person.Fnr
 import no.nav.veilarbaktivitet.person.UserInContext
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
+import java.time.ZonedDateTime
 import java.util.*
 
 @RestController
@@ -24,32 +23,52 @@ class ArkiveringsController(
     private val navnService: EksternNavnService,
     private val appService: AktivitetAppService
 ) {
-    @PostMapping
-    @AuthorizeFnr(auditlogMessage = "arkivere aktivitetsplan og dialog")
-    fun arkiverAktivitetsplanOgDialog(@RequestParam("oppfolgingsperiodeId") oppfølgingsperiodeId: UUID) {
+    @GetMapping("/forhaandsvisning")
+    @AuthorizeFnr(auditlogMessage = "lag forhåndsvisning av aktivitetsplan og dialog")
+    fun forhaandsvisAktivitetsplanOgDialog(@RequestParam("oppfolgingsperiodeId") oppfølgingsperiodeId: UUID): ForhaandsvisningOutboundDTO {
+        val dataHentet = ZonedDateTime.now()
         val fnr = userInContext.fnr.get()
+        val navn = hentNavn(fnr)
+        val aktiviteter = appService.hentAktiviteterForIdentMedTilgangskontroll(fnr)
+        val dialoger = dialogClient.hentDialoger(fnr)
+
+        val payload = lagArkivPayload(fnr, navn, oppfølgingsperiodeId, aktiviteter, dialoger)
+        val forhaandsvisningResultat = orkivarClient.hentPdfForForhaandsvisning(payload)
+
+        return ForhaandsvisningOutboundDTO(
+            forhaandsvisningResultat.pdf,
+            dataHentet
+        )
+    }
+
+    @PostMapping("/journalfor")
+    @AuthorizeFnr(auditlogMessage = "journalføre aktivitetsplan og dialog")
+    fun arkiverAktivitetsplanOgDialog(@RequestParam("oppfolgingsperiodeId") oppfølgingsperiodeId: UUID, @RequestBody arkiverInboundDTO: ArkiverInboundDTO) {
+        val fnr = userInContext.fnr.get()
+
+        val aktiviteter = appService.hentAktiviteterForIdentMedTilgangskontroll(fnr)
+        val dialoger = dialogClient.hentDialoger(fnr)
+        val navn = hentNavn(fnr)
+
+        val oppdatertEtterForhaandsvisning = aktiviteterOgDialogerOppdatertEtter(arkiverInboundDTO.forhaandsvisningOpprettet, aktiviteter, dialoger)
+        if(oppdatertEtterForhaandsvisning) throw ResponseStatusException(HttpStatus.CONFLICT)
+
+        val payload = lagArkivPayload(fnr, navn, oppfølgingsperiodeId, aktiviteter, dialoger)
+        orkivarClient.journalfor(payload)
+    }
+
+    private fun hentNavn(fnr: Fnr): Navn {
         val result = navnService.hentNavn(fnr)
         if(result.errors?.isNotEmpty() == true) { throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR) }
-        val navn = result.data.hentPerson.navn.first().tilFornavnMellomnavnEtternavn()
-
-        val aktiviteterIOppfølgingsperioden = appService.hentAktiviteterForIdentMedTilgangskontroll(fnr)
-            .filter { it.oppfolgingsperiodeId == oppfølgingsperiodeId }
-        val dialogerIOppfølgingsperioden = dialogClient.hentDialoger(fnr)
-            .filter { it.oppfolgingsperiode == oppfølgingsperiodeId }
-        val aktivitetDialoger = dialogerIOppfølgingsperioden.groupBy { it.aktivitetId }
-
-        val aktiviteterPayload = aktiviteterIOppfølgingsperioden
-            .map { it ->
-                val meldingerTilhørendeAktiviteten = aktivitetDialoger[it.id.toString()]?.map {
-                    it.meldinger.map { it.tilMelding() }
-                }?.flatten() ?: emptyList()
-
-                it.toArkivPayload(
-                    meldinger = meldingerTilhørendeAktiviteten
-                )
-            }
-
-        val meldingerUtenAktivitet = aktivitetDialoger[null] ?: emptyList()
-        orkivarClient.arkiver(fnr, navn, aktiviteterPayload, meldingerUtenAktivitet.map { it.tilDialogTråd() })
+        return result.data.hentPerson.navn.first()
     }
+
+    data class ForhaandsvisningOutboundDTO(
+        val pdf: ByteArray,
+        val forhaandsvisningOpprettet: ZonedDateTime
+    )
+
+    data class ArkiverInboundDTO(
+        val forhaandsvisningOpprettet: ZonedDateTime
+    )
 }
