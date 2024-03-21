c6e54a4cf2e90ec6f6f462a34d59f74ddd5fdf96
Mads Lee Giil <mads.lee.giil@nav.no>
2024-03-21 10:37:35 +0100

    Lag skjellet for å opprette endring mellom to versjoner av en aktivitet

--- src/main/kotlin/no/nav/veilarbaktivitet/aktivitet/HistorikkService.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/aktivitet/HistorikkService.kt
@@ -1,5 +1,8 @@
 package no.nav.veilarbaktivitet.aktivitet
 
+import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData
+import no.nav.veilarbaktivitet.person.Innsender
+import no.nav.veilarbaktivitet.util.DateUtils
 import org.springframework.stereotype.Service
 import java.time.ZonedDateTime
 
@@ -8,23 +11,47 @@ class HistorikkService(
     private val aktivitetDAO: AktivitetDAO
 ) {
 
-    fun hentHistorikk(aktivitetIder: List<Long>): Map<Long, Historikk> {
-        aktivitetDAO.hentAktivitetVersjoner(aktivitetIder)
+    fun hentHistorikk(aktivitetIder: List<AktivitetId>): Map<AktivitetId, Historikk> {
+        val aktivitetVersjoner: Map<AktivitetId, List<AktivitetData>> = aktivitetDAO.hentAktivitetVersjoner(aktivitetIder)
+
+        return aktivitetVersjoner.map { (aktivitetId, aktivitetVersjoner) ->
+            val sortereteAktivitetVersjoner = aktivitetVersjoner.sortedBy { it.versjon }
+            val endringer = sortereteAktivitetVersjoner.mapIndexedNotNull { index, aktivitetData ->
+                if (index > 0) {
+                    val forrigeAktivitetVersjon = sortereteAktivitetVersjoner[index]
+                    lagEndring(forrigeAktivitetVersjon, aktivitetData)
+                } else {
+                    null
+                }
+            }
+            aktivitetId to Historikk(endringer = endringer)
+        }.toMap()
+    }
+
+    private fun lagEndring(forrige: AktivitetData, oppdatert: AktivitetData): Endring {
+        return Endring(
+            endretAvType = oppdatert.endretAvType,
+            endretAv = "",
+            tidspunkt = DateUtils.dateToZonedDateTime(oppdatert.endretDato),
+            beskrivelse = ""
+        )
     }
 }
 
+typealias AktivitetId = Long
 
 data class Historikk(
     val endringer: List<Endring>
 )
 
 data class Endring(
-    val endretAvType: BrukerType,
+    val endretAvType: Innsender,
     val endretAv: String?,
     val tidspunkt: ZonedDateTime,
     val beskrivelse: String,
-
 )
+
+// TODO: Ligger et enum med samme konstanter i person/Innsender
 enum class BrukerType {
     BRUKER,
     ARBEIDSGIVER,
