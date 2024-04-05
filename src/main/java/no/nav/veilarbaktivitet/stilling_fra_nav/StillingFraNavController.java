7e9dc48a1908d35b559325bc062c52ec8be61f5a
sigurdgroneng <sigurd.groneng@nav.no>
2024-04-05 16:01:51 +0200

    Bruk annotasjon i StillingFraNavController

--- src/main/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavController.java
+++ src/main/java/no/nav/veilarbaktivitet/stilling_fra_nav/StillingFraNavController.java
@@ -1,12 +1,14 @@
 package no.nav.veilarbaktivitet.stilling_fra_nav;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.poao.dab.spring_a2_annotations.auth.AuthorizeFnr;
 import no.nav.poao.dab.spring_auth.IAuthService;
 import no.nav.veilarbaktivitet.aktivitet.AktivitetAppService;
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData;
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetTypeData;
 import no.nav.veilarbaktivitet.aktivitet.dto.AktivitetDTO;
 import no.nav.veilarbaktivitet.aktivitet.mappers.AktivitetDTOMapper;
+import no.nav.veilarbaktivitet.config.AktivitetResource;
 import no.nav.veilarbaktivitet.person.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,32 +27,23 @@ public class StillingFraNavController {
     private final DelingAvCvService service;
 
     @PutMapping("/kanDeleCV")
-    public AktivitetDTO oppdaterKanCvDeles(@RequestParam long aktivitetId, @RequestBody DelingAvCvDTO delingAvCvDTO) {
+    @AuthorizeFnr(auditlogMessage = "oppdater kan dele CV", resourceIdParamName = "aktivitetId", resourceType = AktivitetResource.class)
+    public AktivitetDTO oppdaterKanCvDeles(@RequestParam("aktivitetId") String aktivitetId, @RequestBody DelingAvCvDTO delingAvCvDTO) {
         boolean erEksternBruker = authService.erEksternBruker();
-        var aktivitet = aktivitetAppService.hentAktivitet(aktivitetId);
-
-        authService.sjekkTilgangTilPerson(aktivitet.getAktorId().eksternBrukerId());
-
-
+        var aktivitet = aktivitetAppService.hentAktivitet(Long.parseLong(aktivitetId));
         if (aktivitet.getAktivitetType() != AktivitetTypeData.STILLING_FRA_NAV) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Kan bare dele cv på aktiviteter med type %s", AktivitetTypeData.STILLING_FRA_NAV));
         }
-
         if (aktivitet.getStillingFraNavData().getSvarfrist().toInstant().isBefore(Instant.now())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Svarfrist %s er utløpt.", aktivitet.getStillingFraNavData().getSvarfrist()));
         }
-
         if (aktivitet.getVersjon() != delingAvCvDTO.aktivitetVersjon) {
             throw new ResponseStatusException(HttpStatus.CONFLICT,
                     String.format("Kan ikke dele cv på aktivitetversjon: %s når siste versjon er: %s", delingAvCvDTO.aktivitetVersjon, aktivitet.getVersjon()));
-
         }
-
         if (!erEksternBruker && delingAvCvDTO.getAvtaltDato() == null) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "avtaltDato er påkrevd når veileder svarer");
         }
-
-
         return Optional.of(aktivitet)
                 .map(a -> service.behandleSvarPaaOmCvSkalDeles(a, delingAvCvDTO.kanDeles, delingAvCvDTO.avtaltDato, erEksternBruker))
                 .map(a -> AktivitetDTOMapper.mapTilAktivitetDTO(a, erEksternBruker))
@@ -58,15 +51,12 @@ public class StillingFraNavController {
     }
 
     @PutMapping("/soknadStatus")
-    public AktivitetDTO oppdaterSoknadstatus(@RequestParam long aktivitetId, @RequestBody SoknadsstatusDTO soknadsstatusDTO) {
+    @AuthorizeFnr(auditlogMessage = "oppdater soknadStatus", resourceIdParamName = "aktivitetId", resourceType = AktivitetResource.class)
+    public AktivitetDTO oppdaterSoknadstatus(@RequestParam("aktivitetId") String aktivitetId, @RequestBody SoknadsstatusDTO soknadsstatusDTO) {
         boolean erEksternBruker = authService.erEksternBruker();
-        var aktivitet = aktivitetAppService
-                .hentAktivitet(aktivitetId);
-
+        var aktivitet = aktivitetAppService.hentAktivitet(Long.parseLong(aktivitetId));
         authService.sjekkTilgangTilPerson(aktivitet.getAktorId().eksternBrukerId());
-
         kanEndreAktivitetSoknadsstatusGuard(aktivitet, soknadsstatusDTO.getAktivitetVersjon());
-
         return Optional.of(aktivitet)
                 .map(a -> service.oppdaterSoknadsstatus(a, soknadsstatusDTO.getSoknadsstatus(), Person.of(authService.getLoggedInnUser())))
                 .map(a -> AktivitetDTOMapper.mapTilAktivitetDTO(a, erEksternBruker))
