8823195fd7a6ffdaeba4fb55d09b5bfc0314a9f5
Mads Lee Giil <mads.lee.giil@nav.no>
2024-04-22 12:31:34 +0200

    Legg til flere testcases

--- src/test/kotlin/no/nav/veilarbaktivitet/arena/VeilarbArenaMapperTest.kt
+++ src/test/kotlin/no/nav/veilarbaktivitet/arena/VeilarbArenaMapperTest.kt
@@ -1,17 +1,16 @@
 package no.nav.veilarbaktivitet.arena
 
 import no.nav.veilarbaktivitet.arena.model.AktiviteterDTO
-import no.nav.veilarbaktivitet.internapi.InternAktivitetMapper.mapTilAktivitet
 import no.nav.veilarbaktivitet.oppfolging.periode.Oppfolgingsperiode
-import no.nav.veilarbaktivitet.testutils.ArenaAktivitetUtils.createTiltaksaktivitet
+import no.nav.veilarbaktivitet.testutils.ArenaAktivitetUtils.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.ZonedDateTime
 import java.util.*
 
 class VeilarbArenaMapperTest {
+
     @Test
     fun `En tiltaksaktivitet har en oppfølgingsperiode hvis sistEndret er innenfor en oppfølgingsperiode`() {
         val oppfølgingsperiode = Oppfolgingsperiode(
@@ -45,7 +44,51 @@ class VeilarbArenaMapperTest {
     }
 
     @Test
-    fun `En tiltaksaktivitet har en oppfølgingsperiode hvis sistEndret er innenfor en oppfølgingsperiode selv om tilOgMedDato er utafor`() {
+    fun `En utdanningsaktivitet har en oppfølgingsperiode hvis tilOgMed er innenfor en oppfølgingsperiode`() {
+        val oppfølgingsperiode = Oppfolgingsperiode(
+            "123",
+            UUID.randomUUID(),
+            ZonedDateTime.now().minusMonths(1),
+            ZonedDateTime.now()
+        )
+        val tiltaksaktivitet = createUtdanningsaktivitet().apply {
+            this.aktivitetPeriode.tom = LocalDate.now().minusDays(1)
+        }
+
+        val mappetAktivitet = VeilarbarenaMapper.mapTilAktivitet(tiltaksaktivitet, listOf(oppfølgingsperiode))
+
+        assertThat(mappetAktivitet.oppfolgingsperiodeId).isEqualTo(oppfølgingsperiode.oppfolgingsperiodeId)
+    }
+
+    @Test
+    fun `En gruppeaktivitet har en oppfølgingsperiode hvis tilOgMed på siste møteplan er innenfor en oppfølgingsperiode`() {
+        val oppfølgingsperiode = Oppfolgingsperiode(
+            "123",
+            UUID.randomUUID(),
+            ZonedDateTime.now().minusMonths(1),
+            ZonedDateTime.now()
+        )
+        val tiltaksaktivitet = createGruppeaktivitet().apply {
+            this.moteplanListe = listOf(
+                AktiviteterDTO.Gruppeaktivitet.Moteplan().apply {
+                    this.startDato = LocalDate.now().minusYears(2)
+                    this.sluttDato = LocalDate.now().minusYears(1)
+                },
+                AktiviteterDTO.Gruppeaktivitet.Moteplan().apply {
+                    this.startDato = LocalDate.now().minusMonths(2)
+                    this.sluttDato = LocalDate.now().minusDays(1)
+                }
+            )
+        }
+
+        val mappetAktivitet = VeilarbarenaMapper.mapTilAktivitet(tiltaksaktivitet, listOf(oppfølgingsperiode))
+
+        assertThat(mappetAktivitet.oppfolgingsperiodeId).isEqualTo(oppfølgingsperiode.oppfolgingsperiodeId)
+    }
+
+
+    @Test
+    fun `En aktivitet har en oppfølgingsperiode hvis sistEndret er innenfor en oppfølgingsperiode selv om tilOgMedDato er utafor`() {
         val oppfølgingsperiode = Oppfolgingsperiode(
             "123",
             UUID.randomUUID(),
@@ -63,7 +106,7 @@ class VeilarbArenaMapperTest {
     }
 
     @Test
-    fun `En tiltaksaktivitet tilhører oppfølgingsperioden som sistEndret er innenfor, og ikke den tilOgMedDato er innenfor`() {
+    fun `En aktivitet tilhører oppfølgingsperioden som sistEndret er innenfor, og ikke den tilOgMedDato er innenfor`() {
         val nyOppfølgingsperiode = Oppfolgingsperiode(
             "123",
             UUID.randomUUID(),
@@ -76,7 +119,6 @@ class VeilarbArenaMapperTest {
             ZonedDateTime.now().minusMonths(3),
             ZonedDateTime.now().minusMonths(5)
         )
-
         val tiltaksaktivitet = createTiltaksaktivitet().apply {
             this.statusSistEndret = LocalDate.now().minusDays(1)
             this.deltakelsePeriode.tom = LocalDate.now().minusMonths(4)
