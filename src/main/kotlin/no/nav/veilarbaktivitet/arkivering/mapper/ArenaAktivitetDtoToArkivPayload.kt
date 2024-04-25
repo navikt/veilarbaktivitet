5359272dade6d9823fa5a45f63beef82492d7d4c
johannetronstad <johanne.tronstad@nav.no>
2024-04-25 15:09:46 +0200

    Map tiltaksaktivitetdetaljer

--- src/main/kotlin/no/nav/veilarbaktivitet/arkivering/mapper/ArenaAktivitetDtoToArkivPayload.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/arkivering/mapper/ArenaAktivitetDtoToArkivPayload.kt
@@ -2,11 +2,10 @@ package no.nav.veilarbaktivitet.arkivering.mapper
 
 import no.nav.veilarbaktivitet.arena.model.ArenaAktivitetDTO
 import no.nav.veilarbaktivitet.arena.model.ArenaAktivitetTypeDTO
-import no.nav.veilarbaktivitet.arkivering.AktivitetHistorikk
-import no.nav.veilarbaktivitet.arkivering.ArkivAktivitet
-import no.nav.veilarbaktivitet.arkivering.Melding
+import no.nav.veilarbaktivitet.arkivering.*
 import no.nav.veilarbaktivitet.arkivering.etiketter.ArkivEtikett
 import no.nav.veilarbaktivitet.arkivering.etiketter.ArkivEtikettStil
+import no.nav.veilarbaktivitet.util.DateUtils.norskDato
 
 fun ArenaAktivitetDTO.toArkivPayload(meldinger: List<Melding>): ArkivAktivitet =
     ArkivAktivitet(
@@ -27,6 +26,43 @@ fun ArenaAktivitetDTO.toArkivTypeTekst() =
         ArenaAktivitetTypeDTO.UTDANNINGSAKTIVITET -> "Utdanning"
     }
 
+fun ArenaAktivitetDTO.toDetaljer(): List<Detalj> {
+    val fellesDetaljer = listOf(
+        Detalj(Stil.PARAGRAF, "Fullført / Tiltak gjennom NAV", tekst = this.beskrivelse),
+        Detalj(Stil.HALV_LINJE, "Fra dato", tekst = norskDato(this.fraDato) ),
+        Detalj(Stil.HALV_LINJE, "Til dato", tekst = norskDato(this.tilDato) ))
+//    val forhaandsorienteringDetalj = Detalj(Stil.PARAGRAF, "Forhåndsorientering", tekst = this.forhaandsorientering) TODO
+
+    return when (type) {
+        ArenaAktivitetTypeDTO.TILTAKSAKTIVITET -> fellesDetaljer + toTiltaksaktivitetDetaljer()
+        ArenaAktivitetTypeDTO.GRUPPEAKTIVITET -> fellesDetaljer + toGruppeaktivitetDetaljer()
+        ArenaAktivitetTypeDTO.UTDANNINGSAKTIVITET -> fellesDetaljer
+    }
+}
+
+fun ArenaAktivitetDTO.toTiltaksaktivitetDetaljer(): List<Detalj> {
+    return listOf(
+        Detalj(Stil.HALV_LINJE, "Deltakelsesprosent", tekst = "${this.deltakelseProsent}%"),
+        Detalj(Stil.HALV_LINJE, "Arrangør", tekst = this.arrangoer),
+        Detalj(Stil.HALV_LINJE, "Dager per uke", tekst = "${this.antallDagerPerUke} dager"),
+
+        )
+}
+
+fun ArenaAktivitetDTO.toGruppeaktivitetDetaljer(): List<Detalj> {
+    val møteplanTekst = this.moeteplanListe.map {
+        if(it.sluttDato.equals(it.startDato) ) {
+            " - ${norskDato(it.startDato)} - ${norskDato(it.sluttDato)} ${it.sted}"
+    } else {
+            " - ${norskDato(it.startDato)} ${it.sted}"
+        }
+    }.joinToString(separator = "\n")
+    return listOf(
+        Detalj(Stil.PARAGRAF, "Tidspunkt og sted", tekst = møteplanTekst)
+    )
+}
+
+// Skal vi se på avtalt feltet?
 // Kan dette være riktig?
 //
 //fun ArenaAktivitetDTO.toArkivEtikett() =
