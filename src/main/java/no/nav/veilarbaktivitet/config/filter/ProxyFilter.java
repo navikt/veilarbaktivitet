d11d46c7aa647d2d643ca7843a3f8d9ddc87b91a
Mads Lee Giil <mads.lee.giil@nav.no>
2024-07-08 15:40:12 +0200

    WIP: PRoxy

--- src/main/java/no/nav/veilarbaktivitet/config/filter/ProxyFilter.java
+++ src/main/java/no/nav/veilarbaktivitet/config/filter/ProxyFilter.java
@@ -3,11 +3,13 @@ package no.nav.veilarbaktivitet.config.filter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener;
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import no.nav.common.rest.client.RestClient;
 import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient;
 import no.nav.common.token_client.client.MachineToMachineTokenClient;
 import no.nav.common.token_client.client.TokenXOnBehalfOfTokenClient;
+import no.nav.common.utils.EnvironmentUtils;
 import no.nav.poao.dab.spring_auth.IAuthService;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -18,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.http.HttpClient;
 import java.util.function.Supplier;
 
@@ -48,24 +52,15 @@ public class ProxyFilter implements Filter {
 
     private final Supplier<String> machineToMachineTokenSupplier = () -> machineToMachineTokenClient.createMachineToMachineToken(scope);
 
+    @Autowired
+    private final OkHttpClient proxyHttpClient;
 
     @Bean
-    OkHttpClient proxyHttpClient(MeterRegistry meterRegistry, AzureAdOnBehalfOfTokenClient azureAdOnBehalfOfTokenClient) {
+    OkHttpClient proxyHttpClient(MeterRegistry meterRegistry, Interceptor tokenInterceptor) {
         return RestClient.baseClientBuilder()
-                .addInterceptor(azureAdInterceptor(() ->
-                        azureAdOnBehalfOfTokenClient.exchangeOnBehalfOfToken(dialogScope, authService.getInnloggetBrukerToken())
-                )).eventListener(OkHttpMetricsEventListener.builder(meterRegistry, "okhttp.requests").build())
+                .addInterceptor(tokenInterceptor)
+                .eventListener(OkHttpMetricsEventListener.builder(meterRegistry, "okhttp.requests").build())
                 .build();
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        return RestClient.baseClientBuilder()
-                .addInterceptor(tokenInterceptor())
-                .build();
-        // veksle token
-        // sende request til veilarbaktivitet i gcp
-        // returnere svaret til clienten
     }
 
     @Bean
@@ -85,6 +80,31 @@ public class ProxyFilter implements Filter {
         };
     }
 
+    Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(getHost(), 80));
+
+    OkHttpClient client = new OkHttpClient.Builder()
+            .proxy(proxy)
+            .build();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        Request request = new Request.Builder()
+                .url(servletRequest.)
+                .build();
+        // execute the request and obtain the response
+        try (Response response = client.newCall(request).execute()) {
+            // return the response body as a string
+            return response.body().string();
+        }
+    }
+
+    private Request copy(HttpServletRequest request) {
+        return new Request
+    }
+
+
+
     private String getToken() throws Exception {
         if(authService.erInternBruker()) {
             return azureAdTokenSupplier.get();
@@ -96,14 +116,16 @@ public class ProxyFilter implements Filter {
             throw new Exception("Fant ikke supplier for proxykall. Dette burde ikke skje");
         }
     }
-//    @Bean
-//    OkHttpClient proxyHttpClient(MeterRegistry meterRegistry, AzureAdOnBehalfOfTokenClient azureAdOnBehalfOfTokenClient) {
-//        return RestClient.baseClientBuilder()
-//                .addInterceptor(azureAdInterceptor(() ->
-//                        azureAdOnBehalfOfTokenClient.exchangeOnBehalfOfToken(dialogScope, authService.getInnloggetBrukerToken())
-//                )).eventListener(OkHttpMetricsEventListener.builder(meterRegistry, "okhttp.requests").build())
-//                .build();
-//    }
+
+    private String getHost() {
+        if (EnvironmentUtils.isProduction().orElse(false)) {
+            return "https://veilarbaktivitet.prod-fss-pub.nais.io";
+        } else if (EnvironmentUtils.isDevelopment().orElse(false)) {
+            return "https://veilarbaktivitet.dev-fss-pub.nais.io";
+        } else {
+            return "http://localhost";
+        }
+    }
 }
 
 
