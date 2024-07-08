18dbcc3534e75d785d86559855af2e364f178102
sigurdgroneng <sigurd.groneng@nav.no>
2024-07-08 16:06:32 +0200

    Beanify ProxyFilter deps

--- src/main/java/no/nav/veilarbaktivitet/config/filter/ProxyFilter.java
+++ src/main/java/no/nav/veilarbaktivitet/config/filter/ProxyFilter.java
@@ -3,9 +3,8 @@ package no.nav.veilarbaktivitet.config.filter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.okhttp3.OkHttpMetricsEventListener;
 import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import no.nav.common.rest.client.RestClient;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient;
 import no.nav.common.token_client.client.MachineToMachineTokenClient;
 import no.nav.common.token_client.client.TokenXOnBehalfOfTokenClient;
@@ -22,7 +21,6 @@ import org.springframework.context.annotation.Profile;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
-import java.net.http.HttpClient;
 import java.util.function.Supplier;
 
 import static no.nav.common.utils.EnvironmentUtils.isProduction;
@@ -30,6 +28,7 @@ import static no.nav.common.utils.EnvironmentUtils.isProduction;
 @Profile("!test")
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class ProxyFilter implements Filter {
 
     private final String scope = String.format("api://%s-gcp.dab.veilarbaktivitet/.default", isProduction().orElse(false) ? "prod" : "dev");
@@ -38,39 +37,44 @@ public class ProxyFilter implements Filter {
     private final IAuthService authService;
 
     @Autowired
-    private final AzureAdOnBehalfOfTokenClient azureAdOnBehalfOfTokenClient;
-
-    @Autowired
-    private final MachineToMachineTokenClient machineToMachineTokenClient;
-
-    @Autowired
-    private final TokenXOnBehalfOfTokenClient tokenXOnBehalfOfTokenClient;
-
-    private final Supplier<String> azureAdTokenSupplier = () -> azureAdOnBehalfOfTokenClient.exchangeOnBehalfOfToken(scope, authService.getInnloggetBrukerToken());
-
-    private final Supplier<String> tokenXSupplier = () -> tokenXOnBehalfOfTokenClient.exchangeOnBehalfOfToken(scope, authService.getInnloggetBrukerToken());
-
-    private final Supplier<String> machineToMachineTokenSupplier = () -> machineToMachineTokenClient.createMachineToMachineToken(scope);
-
-    @Autowired
     private final OkHttpClient proxyHttpClient;
 
     @Bean
     OkHttpClient proxyHttpClient(MeterRegistry meterRegistry, Interceptor tokenInterceptor) {
-        return RestClient.baseClientBuilder()
+
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(getHost(), 80));
+        OkHttpClient client = new OkHttpClient.Builder()
+                .proxy(proxy)
                 .addInterceptor(tokenInterceptor)
                 .eventListener(OkHttpMetricsEventListener.builder(meterRegistry, "okhttp.requests").build())
                 .build();
+        return client;
     }
 
     @Bean
-    protected Interceptor tokenInterceptor() {
+    protected Interceptor tokenInterceptor(AzureAdOnBehalfOfTokenClient azureAdOnBehalfOfTokenClient, MachineToMachineTokenClient machineToMachineTokenClient, TokenXOnBehalfOfTokenClient tokenXOnBehalfOfTokenClient) {
+        final Supplier<String> azureAdTokenSupplier = () -> azureAdOnBehalfOfTokenClient.exchangeOnBehalfOfToken(scope, authService.getInnloggetBrukerToken());
+        final Supplier<String> tokenXSupplier = () -> tokenXOnBehalfOfTokenClient.exchangeOnBehalfOfToken(scope, authService.getInnloggetBrukerToken());
+        final Supplier<String> machineToMachineTokenSupplier = () -> machineToMachineTokenClient.createMachineToMachineToken(scope);
+
+        Supplier<String> tokenSupplier = () -> {
+            if(authService.erInternBruker()) {
+                return azureAdTokenSupplier.get();
+            } else if (authService.erEksternBruker()) {
+                return tokenXSupplier.get();
+            } else if (authService.erSystemBruker()) {
+                return machineToMachineTokenSupplier.get();
+            } else {
+                throw new RuntimeException("Fant ikke supplier for proxykall. Dette burde ikke skje");
+            }
+        };
+
         return chain -> {
             Request original = chain.request();
             Request newReq = null;
             try {
                 newReq = original.newBuilder()
-                        .addHeader("Authorization", "Bearer " + getToken())
+                        .addHeader("Authorization", "Bearer " + tokenSupplier.get())
                         .method(original.method(), original.body())
                         .build();
             } catch (Exception e) {
@@ -80,40 +84,15 @@ public class ProxyFilter implements Filter {
         };
     }
 
-    Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(getHost(), 80));
-
-    OkHttpClient client = new OkHttpClient.Builder()
-            .proxy(proxy)
-            .build();
-
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
         Request request = new Request.Builder()
-                .url(servletRequest.)
+                .url(servletRequest.getRemoteHost())
                 .build();
-        // execute the request and obtain the response
-        try (Response response = client.newCall(request).execute()) {
-            // return the response body as a string
-            return response.body().string();
-        }
-    }
-
-    private Request copy(HttpServletRequest request) {
-        return new Request
-    }
-
-
-
-    private String getToken() throws Exception {
-        if(authService.erInternBruker()) {
-            return azureAdTokenSupplier.get();
-        } else if (authService.erEksternBruker()) {
-            return tokenXSupplier.get();
-        } else if (authService.erSystemBruker()) {
-            return machineToMachineTokenSupplier.get();
-        } else {
-            throw new Exception("Fant ikke supplier for proxykall. Dette burde ikke skje");
+        try (var response = proxyHttpClient.newCall(request).execute()) {
+            log.info("Proxy ok");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
         }
     }
 
