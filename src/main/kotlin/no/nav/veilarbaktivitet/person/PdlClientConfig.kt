9d23d4b7b23c9ad9174bd4005b6e6539499e2a61
johannetronstad <johanne.tronstad@nav.no>
2024-07-12 12:06:29 +0200

    Inject pdlurl og scope

--- src/main/kotlin/no/nav/veilarbaktivitet/person/PdlClientConfig.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/person/PdlClientConfig.kt
@@ -4,14 +4,21 @@ import no.nav.common.client.pdl.PdlClient
 import no.nav.common.client.pdl.PdlClientImpl
 import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient
 import no.nav.poao.dab.spring_auth.IAuthService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 open class PdlClientConfig(val authService: IAuthService) {
 
+    @Value("\${pdl.url}")
+    private val pdlUrl: String? = null
+
+    @Value("\${pdl.scope}")
+    private val pdlTokenscope: String? = null
+
     @Bean
-    open fun pdlClient(pdlUrl: String?, tokenClient: AzureAdOnBehalfOfTokenClient, pdlTokenscope: String): PdlClient {
+    open fun pdlClient(tokenClient: AzureAdOnBehalfOfTokenClient): PdlClient {
         return PdlClientImpl(pdlUrl, {tokenClient.exchangeOnBehalfOfToken(pdlTokenscope, authService.getInnloggetBrukerToken())}, "B579" )
     }
 }
