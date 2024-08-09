b211ac57a481971f03ed4dd7d4b8aaee0e348a3c
sigurdgroneng <sigurd.groneng@nav.no>
2024-08-09 16:41:39 +0200

    Non concurrent index

--- src/main/resources/db/migration/V3__aktivitet_brukernotifikasjon_unique_index.sql
+++ src/main/resources/db/migration/V3__aktivitet_brukernotifikasjon_unique_index.sql
@@ -1,2 +1,3 @@
-create unique index concurrently if not exists aktivitet_brukernotifikasjon_pk
+-- Ble lagd med concurrently i prod med det funker ikke så bra i testene
+create unique index if not exists aktivitet_brukernotifikasjon_pk
     on aktivitet_brukernotifikasjon (aktivitet_id, brukernotifikasjon_id);
