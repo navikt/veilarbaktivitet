1eed298073d95adc235931a7fd40b077e8cd315c
Mads Lee Giil <mads.lee.giil@nav.no>
2024-09-05 09:03:08 +0200

    Håndter at lazy initialisering ikke alltid fungerte

--- src/test/kotlin/no/nav/veilarbaktivitet/admin/KasserControllerTest.kt
+++ src/test/kotlin/no/nav/veilarbaktivitet/admin/KasserControllerTest.kt
@@ -4,24 +4,37 @@ import no.nav.veilarbaktivitet.SpringBootTestBase
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetStatus
 import no.nav.veilarbaktivitet.aktivitet.dto.AktivitetTypeDTO
 import no.nav.veilarbaktivitet.mock_nav_modell.BrukerOptions
+import no.nav.veilarbaktivitet.mock_nav_modell.MockBruker
+import no.nav.veilarbaktivitet.mock_nav_modell.MockVeileder
 import no.nav.veilarbaktivitet.testutils.AktivitetDtoTestBuilder
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.http.HttpStatus
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class KasserControllerTest : SpringBootTestBase() {
-    private val mockBruker by lazy { navMockService.createHappyBruker(BrukerOptions.happyBruker()) }
-    private val veileder by lazy { navMockService.createVeileder(ident = "Z999999", mockBruker = mockBruker) }
+
+    private lateinit var mockBruker: MockBruker
+    private lateinit var veilederSomKanKassere: MockVeileder
+
+    @BeforeAll
+    fun beforeAll() {
+        mockBruker = navMockService.createHappyBruker(BrukerOptions.happyBruker())
+        veilederSomKanKassere = navMockService.createVeileder(ident = "Z999999", mockBruker = mockBruker)
+    }
 
     @Test
     fun `skal kke kunne kassere aktivitet uten tilgang`() {
+        val veilederUtenKasseringstilgang = navMockService.createVeileder(mockBruker = mockBruker)
         val aktivitet = aktivitetTestService.opprettAktivitet(
             mockBruker, AktivitetDtoTestBuilder.nyAktivitet(
                 AktivitetTypeDTO.STILLING
             )
         )
 
-        veileder
+        veilederUtenKasseringstilgang
             .createRequest()
             .put("http://localhost:" + port + "/veilarbaktivitet/api/kassering/" + aktivitet.id)
             .then()
@@ -37,12 +50,12 @@ internal class KasserControllerTest : SpringBootTestBase() {
             )
         )
         val aktivitetId = opprettetAktivitet.id.toLong()
-        val versjonerFørKassering = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veileder)
+        val versjonerFørKassering = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veilederSomKanKassere)
         assertThat(versjonerFørKassering.size).isEqualTo(1)
 
-        aktivitetTestService.kasserAktivitetskort(veileder, opprettetAktivitet.id)
+        aktivitetTestService.kasserAktivitetskort(veilederSomKanKassere, opprettetAktivitet.id)
 
-        val versjonerEtterKassering = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veileder)
+        val versjonerEtterKassering = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veilederSomKanKassere)
         assertThat(versjonerEtterKassering.size).isEqualTo(2)
     }
 
@@ -51,9 +64,9 @@ internal class KasserControllerTest : SpringBootTestBase() {
         val opprettetAktivitet = aktivitetTestService.opprettAktivitet(mockBruker, AktivitetDtoTestBuilder.nyAktivitet(AktivitetTypeDTO.STILLING))
         val aktivitetId = opprettetAktivitet.id
 
-        aktivitetTestService.kasserAktivitetskort(veileder, opprettetAktivitet.id)
+        aktivitetTestService.kasserAktivitetskort(veilederSomKanKassere, opprettetAktivitet.id)
 
-        val versjoner = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veileder)
+        val versjoner = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veilederSomKanKassere)
         versjoner.forEach { aktivitetVersjon ->
             assertThat(aktivitetVersjon.tittel).isEqualTo("Det var skrevet noe feil, og det er nå slettet")
             assertThat(aktivitetVersjon.avsluttetKommentar).isEqualTo("Kassert av NAV")
@@ -64,21 +77,19 @@ internal class KasserControllerTest : SpringBootTestBase() {
 
     @Test
     fun `skal ikke overskrive endret-felter og statusfelt på tidligere versjoner av aktiviteten når man kasserer`() {
-        val veilederSomOppretterAktivitet = navMockService.createVeileder(mockBruker = mockBruker)
-        val opprettetAktivitet = aktivitetTestService.opprettAktivitet(mockBruker, veilederSomOppretterAktivitet, AktivitetDtoTestBuilder.nyAktivitet(AktivitetTypeDTO.STILLING))
+        val annenVeileder = navMockService.createVeileder(mockBruker = mockBruker)
+        val opprettetAktivitet = aktivitetTestService.opprettAktivitet(mockBruker, annenVeileder, AktivitetDtoTestBuilder.nyAktivitet(AktivitetTypeDTO.STILLING))
         val aktivitetId = opprettetAktivitet.id
 
-        aktivitetTestService.kasserAktivitetskort(veileder, opprettetAktivitet.id)
+        aktivitetTestService.kasserAktivitetskort(veilederSomKanKassere, opprettetAktivitet.id)
 
-        val versjoner = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veileder)
+        val versjoner = aktivitetTestService.hentVersjoner(aktivitetId.toString(), mockBruker, veilederSomKanKassere)
         val sorterteVersjoner = versjoner.sortedBy { it.endretDato }
-
         val førsteVersjon = sorterteVersjoner.first()
         assertThat(førsteVersjon.status).isEqualTo(AktivitetStatus.PLANLAGT)
-        assertThat(førsteVersjon.endretAv).isEqualTo(veilederSomOppretterAktivitet.navIdent)
-
+        assertThat(førsteVersjon.endretAv).isEqualTo(annenVeileder.navIdent)
         val andreVersjon = sorterteVersjoner[1]
         assertThat(andreVersjon.status).isEqualTo(AktivitetStatus.AVBRUTT)
-        assertThat(andreVersjon.endretAv).isEqualTo(veileder.navIdent)
+        assertThat(andreVersjon.endretAv).isEqualTo(veilederSomKanKassere.navIdent)
     }
 }
