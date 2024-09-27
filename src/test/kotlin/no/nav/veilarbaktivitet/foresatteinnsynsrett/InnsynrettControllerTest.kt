29f02b0e12b0dbf10d9561a56e765e865f4c7731
Henrik uran <122083502+henriktheboss@users.noreply.github.com>
2024-09-27 12:43:26 +0200

    Rydda opp koden

--- src/test/kotlin/no/nav/veilarbaktivitet/foresatteinnsynsrett/InnsynrettControllerTest.kt
+++ src/test/kotlin/no/nav/veilarbaktivitet/foresatteinnsynsrett/InnsynrettControllerTest.kt
@@ -9,75 +9,75 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 
-internal class InnsynrettControlerKtTest: SpringBootTestBase() {
+internal class InnsynrettControllerTest: SpringBootTestBase() {
     @Test
-    fun `for bruker som er over 18 har ikke foresatte innsyns rett`() {
+    fun `for bruker som er over 18 har ikke foresatte innsynsrett`() {
         val fødselsdato = LocalDate.now().minusYears(18)
         val brukerOptions = BrukerOptions.happyBruker().toBuilder().fnr("${fødselsdato.tilFødselsDato()}60000").build()
         val bruker = navMockService.createHappyBruker(brukerOptions)
 
         val response = bruker
             .createRequest()
-            .get( "http://localhost:$port/veilarbaktivitet/api/innsynsrett")
+            .get( "http://localhost:$port/veilarbaktivitet/api/ekstern/innsynsrett")
             .then()
             .assertThat()
             .statusCode(200)
             .extract()
             .response()
-            .`as`(InnsynrettControler.InnsynsrettDTO::class.java)
+            .`as`(InnsynrettController.InnsynsrettDTO::class.java)
 
         assertThat(response.foresatteHarInnsynsrett).isFalse()
     }
 
     @Test
-    fun `for bruker som er under 18 har foresatte innsyns rett`() {
+    fun `for bruker som er under 18 har foresatte innsynsrett`() {
         val fødselsdato = LocalDate.now().minusYears(18).plusDays(1)
         val brukerOptions = BrukerOptions.happyBruker().toBuilder().fnr("${fødselsdato.tilFødselsDato()}60000").build()
         val bruker = navMockService.createHappyBruker(brukerOptions)
 
         val response = bruker
             .createRequest()
-            .get( "http://localhost:$port/veilarbaktivitet/api/innsynsrett")
+            .get( "http://localhost:$port/veilarbaktivitet/api/ekstern/innsynsrett")
             .then()
             .assertThat()
             .statusCode(200)
             .extract()
             .response()
-            .`as`(InnsynrettControler.InnsynsrettDTO::class.java)
+            .`as`(InnsynrettController.InnsynsrettDTO::class.java)
 
         assertThat(response.foresatteHarInnsynsrett).isTrue()
     }
 
     @Test
-    fun `for bruker som er født på 1900 tallet har foresatte aldri innsyns rett`() {
+    fun `for bruker som er født på 1900 tallet har foresatte aldri innsynsrett`() {
 
         val brukerOptions = BrukerOptions.happyBruker().toBuilder().fnr("01012320000").build()
         val bruker = navMockService.createHappyBruker(brukerOptions)
 
         val response = bruker
             .createRequest()
-            .get( "http://localhost:$port/veilarbaktivitet/api/innsynsrett")
+            .get( "http://localhost:$port/veilarbaktivitet/api/ekstern/innsynsrett")
             .then()
             .assertThat()
             .statusCode(200)
             .extract()
             .response()
-            .`as`(InnsynrettControler.InnsynsrettDTO::class.java)
+            .`as`(InnsynrettController.InnsynsrettDTO::class.java)
 
         assertThat(response.foresatteHarInnsynsrett).isFalse()
     }
 
-//    @Test
-    fun `veilleder skal også kunne sjekke om foresatte har innsyns rett`() {
+    @Test
+    fun `veilleder skal ikke kunne sjekke om foresatte har innsynsrett`() {
         val bruker = navMockService.createHappyBruker()
         val veileder = navMockService.createVeileder("Z123456" ,bruker)
 
         veileder
             .createRequest()
-            .get( "http://localhost:$port/veilarbaktivitet/api/innsynsrett")
+            .get( "http://localhost:$port/veilarbaktivitet/api/ekstern/innsynsrett")
             .then()
             .assertThat()
-            .statusCode(200)
+            .statusCode(403)
     }
 
     fun LocalDate.tilFødselsDato(): String {
