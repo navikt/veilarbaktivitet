77d8707f5b86d6cd830bfc537fc0c3f59d56567e
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2024-12-10 16:22:26 +0100

    Grant usage på schema også

--- src/main/resources/db/migration/V9__grant_lesetilgang_til_clousqliamuser.sql
+++ src/main/resources/db/migration/V9__grant_lesetilgang_til_clousqliamuser.sql
@@ -3,8 +3,9 @@ $$
     BEGIN
         IF (SELECT exists(SELECT rolname FROM pg_roles WHERE rolname = 'cloudsqliamuser'))
         THEN
-            ALTER DEFAULT PRIVILEGES IN SCHEMA veilarbaktivitet GRANT SELECT ON TABLES TO "cloudsqliamuser";
+            GRANT USAGE ON SCHEMA veilarbaktivitet to "cloudsqliamuser";
             GRANT SELECT ON ALL TABLES IN SCHEMA veilarbaktivitet TO "cloudsqliamuser";
+            ALTER DEFAULT PRIVILEGES IN SCHEMA veilarbaktivitet GRANT SELECT ON TABLES TO "cloudsqliamuser";
         END IF;
     END
 $$;
