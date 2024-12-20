46488dd87ce1a76f015a8d29a85fd575f7b8412e
Henrik uran <henrik.uran.nykmark@nav.no>
2024-12-20 12:28:42 +0100

    legg til testCase
    Signed-off-by: Henrik uran <henrik.uran.nykmark@nav.no>

--- src/test/kotlin/no/nav/veilarbaktivitet/oversikten/OversiktenMeldingMedMetadataDAOTest.kt
+++ src/test/kotlin/no/nav/veilarbaktivitet/oversikten/OversiktenMeldingMedMetadataDAOTest.kt
@@ -4,11 +4,13 @@ import no.nav.veilarbaktivitet.SpringBootTestBase
 import no.nav.veilarbaktivitet.aktivitet.dto.AktivitetTypeDTO
 import no.nav.veilarbaktivitet.testutils.AktivitetDtoTestBuilder
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
 open class OversiktenMeldingMedMetadataDAOTest: SpringBootTestBase() {
 
+    @Autowired
+    lateinit var oversiktenService: OversiktenService
     @Autowired
     lateinit var oversiktenMeldingMedMetadataDAO: OversiktenMeldingMedMetadataDAO
 
@@ -24,8 +26,27 @@ open class OversiktenMeldingMedMetadataDAOTest: SpringBootTestBase() {
 
         val resultat = oversiktenMeldingMedMetadataDAO.hentUdelteSamtalereferatDerViIkkeHarSendtMeldingTilOversikten()
         assertThat(resultat).hasSize(1)
-        assertThat(resultat.first().aktorId).isEqualTo(happyBruker.aktorId)
-        assertThat(resultat.first().aktivitetId).isEqualTo(aktivitet.id)
+        assertThat(resultat.first().aktorId.get()).isEqualTo(happyBruker.aktorId.get())
+        assertThat(resultat.first().aktivitetId.toString()).isEqualTo(aktivitet.id)
 
     }
+
+    @Test
+    fun `UdeltSamtalereferat SendtMelding ReturnererIngen`() {
+        val happyBruker = navMockService.createBruker()
+        val veileder = navMockService.createVeileder(happyBruker)
+        val aktivitet = aktivitetTestService.opprettAktivitet(
+            happyBruker,
+            veileder,
+            AktivitetDtoTestBuilder.nyAktivitet(AktivitetTypeDTO.MOTE).setErReferatPublisert(false).setReferat("Et referat")
+        )
+        oversiktenService.lagreStartMeldingOmUdeltSamtalereferatIUtboks(happyBruker.aktorId, aktivitet.id.toLong())
+        oversiktenService.sendUsendteMeldingerTilOversikten()
+
+        val resultat = oversiktenMeldingMedMetadataDAO.hentUdelteSamtalereferatDerViIkkeHarSendtMeldingTilOversikten()
+
+        assertThat(resultat).isEmpty()
+
+    }
+
 }
