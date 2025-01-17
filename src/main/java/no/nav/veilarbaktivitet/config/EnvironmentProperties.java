289f7a8406adcb89c13b249991aeabed78c915f1
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2025-01-17 16:57:12 +0100

    Fjerne abac avhengighet ikke lenger i bruk (#908)

--- src/main/java/no/nav/veilarbaktivitet/config/EnvironmentProperties.java
+++ src/main/java/no/nav/veilarbaktivitet/config/EnvironmentProperties.java
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Setter
 @ConfigurationProperties(prefix = "app.env")
 public class EnvironmentProperties {
-    private String abacUrl;
     private String kafkaBrokersUrl;
     private String environmentName;
     private String cxfStsUrl;
