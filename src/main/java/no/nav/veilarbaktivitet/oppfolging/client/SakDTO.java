4c126b611eda75fe6c889788d5e6034c507fd6af
erikbosnes <erik.bosnes@nav.no>
2025-06-03 14:42:34 +0200

    Fjern ubrukt tema fra sak

--- src/main/java/no/nav/veilarbaktivitet/oppfolging/client/SakDTO.java
+++ src/main/java/no/nav/veilarbaktivitet/oppfolging/client/SakDTO.java
@@ -4,6 +4,5 @@ import java.util.UUID;
 public record SakDTO(
         UUID oppfolgingsperiodeId,
         Long sakId,
-        String fagsaksystem,
-        String tema // TOOD: Ikke bruk denne, vi tror en sak kan ha journalposter på flere forskjellige temaer
+        String fagsaksystem
 ){}
