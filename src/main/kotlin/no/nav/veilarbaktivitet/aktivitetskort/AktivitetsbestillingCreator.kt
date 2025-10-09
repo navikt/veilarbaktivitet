b55758092d91c157abe5c6499b5c23a904c23a29
Erik.Bosnes <erik.bosnes@nav.no>
2025-10-09 09:48:01 +0200

    Validering av tittel cleanup

--- src/main/kotlin/no/nav/veilarbaktivitet/aktivitetskort/AktivitetsbestillingCreator.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/aktivitetskort/AktivitetsbestillingCreator.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.common.client.aktorregister.IngenGjeldendeIdentException
 import no.nav.common.json.JsonMapper
-import no.nav.veilarbaktivitet.aktivitetskort.bestilling.AktivitetskortBestilling
 import no.nav.veilarbaktivitet.aktivitetskort.bestilling.ArenaAktivitetskortBestilling
 import no.nav.veilarbaktivitet.aktivitetskort.bestilling.EksternAktivitetskortBestilling
 import no.nav.veilarbaktivitet.aktivitetskort.dto.AktivitetskortType
@@ -22,7 +21,7 @@ import java.time.ZonedDateTime
 import java.util.*
 
 @Component
-class AktivitetsbestillingCreator (
+class AktivitetsbestillingCreator(
     private val personService: PersonService
 ) {
 
@@ -36,10 +35,16 @@ class AktivitetsbestillingCreator (
         }
     }
 
-    @Throws(DeserialiseringsFeil::class, UgyldigIdentFeil::class, KeyErIkkeFunksjonellIdFeil::class, ValideringFeil::class)
+    @Throws(
+        DeserialiseringsFeil::class,
+        UgyldigIdentFeil::class,
+        KeyErIkkeFunksjonellIdFeil::class,
+        ValideringFeil::class
+    )
     fun lagBestilling(consumerRecord: ConsumerRecord<String, String>, messageId: UUID?): BestillingBase {
         val melding = deserialiser(consumerRecord)
-        val resolvedMessageId = (melding.messageId ?: messageId) ?: throw RuntimeException("Mangler påkrevet messageId på aktivitetskort melding")
+        val resolvedMessageId = (melding.messageId ?: messageId)
+            ?: throw RuntimeException("Mangler påkrevet messageId på aktivitetskort melding")
         if (melding.getAktivitetskortId().toString() != consumerRecord.key()) throw KeyErIkkeFunksjonellIdFeil(
             ErrorMessage(
                 String.format(
@@ -50,10 +55,7 @@ class AktivitetsbestillingCreator (
             ), null
         )
         return if (melding is KafkaAktivitetskortWrapperDTO) {
-            val tittelErGyldig = melding.aktivitetskort.tittel.length <= 255
-            if(!tittelErGyldig) {
-                throw ValideringFeil("Tittelen er for lang")
-            }
+            if (!erAktivitetskortTittelGyldig(melding)) throw ValideringFeil("Tittelen er for lang (over 255 tegn)")
 
             val aktorId = hentAktorId(Person.fnr(melding.aktivitetskort.personIdent))
             val erArenaAktivitet = AktivitetskortType.ARENA_TILTAK == melding.aktivitetskortType
@@ -84,6 +86,8 @@ class AktivitetsbestillingCreator (
             melding
         }
     }
+
+    private fun erAktivitetskortTittelGyldig(melding: KafkaAktivitetskortWrapperDTO): Boolean = melding.aktivitetskort.tittel.length <= 255
 
     companion object {
         private val log = LoggerFactory.getLogger(javaClass)
