047593e82e89412303f48eb5e49db9aa22ffb635
Erik.Bosnes <erik.bosnes@nav.no>
2025-10-16 09:47:37 +0200

    Legg til ukjent verdi

--- src/main/kotlin/no/nav/veilarbaktivitet/arkivering/mapper/AktivitetDtoToArkivPayload.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/arkivering/mapper/AktivitetDtoToArkivPayload.kt
@@ -74,6 +74,7 @@ fun AktivitetData.toArkivTypeTekst(): String {
                 AktivitetskortType.ENKELAMO -> "Arbeidsmarkedsopplæring"
                 AktivitetskortType.ENKFAGYRKE -> "Fag- og yrkesopplæring"
                 AktivitetskortType.HOYERE_UTDANNING -> "Høyere utdanning"
+                AktivitetskortType.UKJENT_VERDI -> "Ukjent verdi"
             }
         }
     }
