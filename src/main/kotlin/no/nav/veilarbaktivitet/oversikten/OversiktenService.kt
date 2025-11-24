1ef16cfda324a51f1b76f8d6e1b908e9998dddb4
johannetronstad <johanne.tronstad@nav.no>
2025-11-24 13:06:08 +0100

    Send usendte meldinger til oversikten hvert minutt

--- src/main/kotlin/no/nav/veilarbaktivitet/oversikten/OversiktenService.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/oversikten/OversiktenService.kt
@@ -23,7 +23,7 @@ open class OversiktenService(
     private val log = LoggerFactory.getLogger(OversiktenService::class.java)
     private val erProd = EnvironmentUtils.isProduction().orElse(false)
 
-    @Scheduled(cron = "0 0 * * * *") // Hver time
+    @Scheduled(cron = "0 * * * * *") // Hvert minutt
     @SchedulerLock(name = "oversikten_melding_med_metadata_scheduledTask", lockAtMostFor = "PT3M")
     open fun sendUsendteMeldingerTilOversikten() {
         val meldingerMedMetadata = oversiktenMeldingMedMetadataRepository.hentAlleSomSkalSendes()
