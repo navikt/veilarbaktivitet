3165806496cafd35a3fde1f9c5d60f31e91fdf84
johannetronstad <johanne.tronstad@nav.no>
2025-12-15 15:00:44 +0100

    Lagre norgkontor i cache

--- src/main/kotlin/no/nav/veilarbaktivitet/norg2/Norg2Client.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/norg2/Norg2Client.kt
@@ -1,39 +1,44 @@
 package no.nav.veilarbaktivitet.norg2
 
 import no.nav.common.client.norg2.Norg2Client
+import no.nav.common.rest.client.RestUtils
+import no.nav.common.types.identer.EnhetId
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
-class Norg2Client(private val norg2HttpClient: Norg2Client) {
+class Norg2Client(private val norg2HttpClient: OkHttpClient) {
 
     private val hentAlleEnheterUrl = "http://norg2.org/norg2/api/v1/enhet"
+    private val log = LoggerFactory.getLogger(Norg2Client::class.java)
+    private var norgKontorCache = mapOf<EnhetId, NorgKontor>()
 
     init {
-        "http://norg2.org/norg2/api/v1/enhet"
+        norgKontorCache = hentAlleEnheter()
     }
 
-    private fun hentAlleEnheter()
-
-
-
-}
-
-/*
-
+    private fun hentAlleEnheter(): Map<EnhetId, NorgKontor> {
         val request: Request = Request.Builder()
             .addHeader("Content-Type", "application/json")
             .addHeader("Accept", "application/json")
-            .post(body)
-            .url("$dialogUrl/veilarbdialog/graphql")
+            .get()
+            .url(hentAlleEnheterUrl)
             .build()
 
-        return try {
-            val response = dialogHttpClient.newCall(request).execute()
-            val responseData: DialogResponse = RestUtils.parseJsonResponseOrThrow(response, DialogResponse::class.java)
-            if(responseData.errors?.isNotEmpty() == true) { throw RuntimeException("Feil ved kall til veilarbdialog") }
-            responseData.data?.dialoger ?: emptyList()
-        } catch (e: Exception) {
-            log.error("Feil ved henting av dialoger fra veilarbdialog", e)
-            throw ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Feil ved henting av dialoger")
+        val response = norg2HttpClient.newCall(request).execute()
+        if (response.isSuccessful) {
+            val responseData = RestUtils.parseJsonArrayResponse(response, NorgKontor::class.java).get()
+            return responseData.associateBy { kontor -> kontor.enhetId }
+        } else {
+            log.error("Feil ved henting av kontor fra norg")
+            throw RuntimeException("Feil ved henting av kontor fra norg")
         }
- */
+    }
+}
+
+data class NorgKontor(
+    val enhetId: EnhetId,
+    val navn: String,
+)
