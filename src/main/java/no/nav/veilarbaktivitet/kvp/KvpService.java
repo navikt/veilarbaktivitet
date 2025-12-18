036189de9613cc653ef961549daa6e2f2139d4e3
sigurdgroneng <sigurd.groneng@nav.no>
2025-12-18 14:12:47 +0100

    Hent kontorsperre fra graphql

--- src/main/java/no/nav/veilarbaktivitet/kvp/KvpService.java
+++ src/main/java/no/nav/veilarbaktivitet/kvp/KvpService.java
@@ -3,8 +3,10 @@ package no.nav.veilarbaktivitet.kvp;
 import jakarta.ws.rs.ForbiddenException;
 import jakarta.ws.rs.InternalServerErrorException;
 import lombok.RequiredArgsConstructor;
+import no.nav.common.client.aktoroppslag.AktorOppslagClient;
+import no.nav.common.types.identer.EnhetId;
+import no.nav.veilarbaktivitet.kvp.v2.KontorSperre;
 import no.nav.veilarbaktivitet.kvp.v2.KvpV2Client;
-import no.nav.veilarbaktivitet.kvp.v2.KvpV2DTO;
 import no.nav.veilarbaktivitet.person.Person;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,24 +19,19 @@ import java.util.Optional;
 public class KvpService {
 
     private final KvpV2Client kvpClient;
+    private final AktorOppslagClient aktorOppslagClient;
 
     public boolean erUnderKvp(Person.AktorId aktorId) {
-        Optional<KvpV2DTO> kvpDTO = kvpClient.get(aktorId);
-
-        if (kvpDTO.isEmpty()) {
-            return false;
-        }
-
-        return kvpDTO
-                .map(it -> it.getEnhet() != null && !it.getEnhet().isEmpty() && it.getAvsluttetDato() == null)
-                .orElse(false);
+        var fnr = aktorOppslagClient.hentFnr(aktorId.otherAktorId());
+        var kvpDTO = kvpClient.get(Person.fnr(fnr.get()));
+        return kvpDTO.isPresent();
     }
 
-
-    public Optional<String> getKontorSperreEnhet(Person.AktorId aktorId) {
+    public Optional<EnhetId> getKontorSperreEnhet(Person.AktorId aktorId) {
+        var fnr = aktorOppslagClient.hentFnr(aktorId.otherAktorId());
         try {
-            Optional<KvpV2DTO> kvp = kvpClient.get(aktorId);
-            return kvp.map(k -> k.getEnhet());
+            Optional<KontorSperre> kvp = kvpClient.get(Person.fnr(fnr.get()));
+            return kvp.map(KontorSperre::getEnhetId);
         } catch (ForbiddenException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "veilarbaktivitet har ikke tilgang til å spørre om KVP-status.");
         } catch (InternalServerErrorException e) {
--- src/main/java/no/nav/veilarbaktivitet/kvp/v2/KvpV2Client.java
+++ src/main/java/no/nav/veilarbaktivitet/kvp/v2/KvpV2Client.java
@@ -5,7 +5,7 @@ import no.nav.veilarbaktivitet.person.Person;
 import java.util.Optional;
 
 public interface KvpV2Client {
-    Optional<KvpV2DTO> get(Person.AktorId aktorId);
+    Optional<KontorSperre> get(Person.Fnr aktorId);
 
     void setBaseUrl(String baseUrl);
 }
