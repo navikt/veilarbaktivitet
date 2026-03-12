2c3ab19c512ee5ac37a26d0dce184f98d3016ba1
johannetronstad <johanne.tronstad@nav.no>
2026-03-12 16:11:24 +0100

    Deaktiver cron job

--- src/main/kotlin/no/nav/veilarbaktivitet/oversikten/OversiktenService.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/oversikten/OversiktenService.kt
@@ -71,8 +71,8 @@ open class OversiktenService(
      * Engangsjobb for å rydde opp i tilfeller der det er sendt START-melding til oversikten,
      * men referatet allerede er publisert (referat_publisert = 1).
      */
-    @Scheduled(cron = "0 6 16 * * ?")
-    @SchedulerLock(name = "oversikten_stopp_publiserte_referat_scheduledTask", lockAtMostFor = "PT15M")
+//    @Scheduled(cron = "0 6 16 * * ?")
+//    @SchedulerLock(name = "oversikten_stopp_publiserte_referat_scheduledTask", lockAtMostFor = "PT15M")
     open fun sendStoppMeldingForPubliserteReferaterMedFeilaktigStartMelding() {
         log.info("Starter opprydding av feilaktige START-meldinger for publiserte referater")
         val feilaktigeStartMeldinger = oversiktenMeldingMedMetadataRepository.hentPubliserteSamtalereferaterMedStartMeldingUtenStoppMelding()
