360fbdb93cea4528f8e5569120ae5faab1f823f7
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-03-31 10:36:36 +0200

    Skill på ulike endringer på møtedata

--- src/main/java/no/nav/veilarbaktivitet/aktivitet/AktivitetService.java
+++ src/main/java/no/nav/veilarbaktivitet/aktivitet/AktivitetService.java
@@ -179,6 +179,39 @@ public class AktivitetService {
         aktivitetDAO.oppdaterAktivitet(oppdatertAktivitetMedNyFrist);
     }
 
+    public void oppdaterMoteTidOgSted(AktivitetData originalAktivitet, AktivitetData aktivitetData) {
+        final var oppdatertAktivitet = originalAktivitet
+                .toBuilder()
+                .endretAvType(aktivitetData.getEndretAvType())
+                .endretAv(aktivitetData.getEndretAv())
+                .endretDato(aktivitetData.getEndretDato())
+                .transaksjonsType(AktivitetTransaksjonsType.MOTE_TID_OG_STED_ENDRET)
+                .fraDato(aktivitetData.getFraDato())
+                .tilDato(aktivitetData.getTilDato())
+                .moteData(ofNullable(originalAktivitet.getMoteData()).map(moteData ->
+                        moteData.withAdresse(aktivitetData.getMoteData().getAdresse())
+                                .withKanal(aktivitetData.getMoteData().getKanal())
+                ).orElse(null))
+                .build();
+        aktivitetDAO.oppdaterAktivitet(oppdatertAktivitet);
+    }
+
+    public void oppdaterMoteDetaljer(AktivitetData originalAktivitet, AktivitetData aktivitetData) {
+        final var oppdatertAktivitet = originalAktivitet
+                .toBuilder()
+                .tittel(aktivitetData.getTittel())
+                .beskrivelse(aktivitetData.getBeskrivelse())
+                .endretAvType(aktivitetData.getEndretAvType())
+                .endretAv(aktivitetData.getEndretAv())
+                .endretDato(aktivitetData.getEndretDato())
+                .transaksjonsType(AktivitetTransaksjonsType.DETALJER_ENDRET)
+                .moteData(ofNullable(originalAktivitet.getMoteData()).map(moteData ->
+                        moteData.withForberedelser(aktivitetData.getMoteData().getForberedelser())
+                ).orElse(null))
+                .build();
+        aktivitetDAO.oppdaterAktivitet(oppdatertAktivitet);
+    }
+
     public AktivitetData oppdaterReferat(
             AktivitetData originalAktivitet,
             AktivitetData aktivitetData
