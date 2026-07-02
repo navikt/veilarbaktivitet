2aba6b7b63af1f00faadf5b1423318fd78314f82
Mads.Lee.Giil <mads.lee.giil@nav.no>
2026-07-02 10:30:40 +0200

    Utled endring på aktivitet

--- src/main/kotlin/no/nav/veilarbaktivitet/aktivitet/HistorikkService.kt
+++ src/main/kotlin/no/nav/veilarbaktivitet/aktivitet/HistorikkService.kt
@@ -1,9 +1,28 @@
 package no.nav.veilarbaktivitet.aktivitet
 
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.AVTALT
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.AVTALT_DATO_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.BLE_HISTORISK
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.DEL_CV_SVART
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.FATT_JOBBEN
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.FORHAANDSORIENTERING_LEST
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.IKKE_FATT_JOBBEN
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.MOTE_KANAL_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.MOTE_STED_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.MOTE_TIDSPUNKT_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.OPPRETTET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.REFERAT_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.REFERAT_OPPRETTET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.REFERAT_PUBLISERT
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.SOKNADSSTATUS_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.STATUS_ENDRET
+import no.nav.veilarbaktivitet.aktivitet.AktivitetendringsType.STILLINGSOK_ETIKETT_ENDRET
 import no.nav.veilarbaktivitet.aktivitet.Målgruppe.*
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetData
 import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetTransaksjonsType
+import no.nav.veilarbaktivitet.aktivitet.domain.AktivitetTypeData
 import no.nav.veilarbaktivitet.person.Innsender
+import no.nav.veilarbaktivitet.stilling_fra_nav.Soknadsstatus
 import no.nav.veilarbaktivitet.util.DateUtils
 import no.nav.veilarbaktivitet.util.DateUtils.norskDato
 import org.springframework.stereotype.Service
@@ -161,4 +180,118 @@ private enum class Målgruppe {
     VEILEDER,
     BRUKER,
     ARKIV
+}
+
+fun utledAktivitetendringsType(forrigeVersjon: AktivitetData?, oppdatertVersjon: AktivitetData): List<AktivitetendringsType> {
+    // Endringer som bare oppstår alene
+    if (forrigeVersjon == null) return listOf(OPPRETTET)
+    if (forrigeVersjon.historiskDato == null && oppdatertVersjon.historiskDato != null) return listOf(BLE_HISTORISK)
+
+    val endringer = mutableListOf<AktivitetendringsType>()
+    if (forrigeVersjon.status != oppdatertVersjon.status) endringer.add(STATUS_ENDRET)
+    if (!forrigeVersjon.isAvtalt && oppdatertVersjon.isAvtalt) endringer.add(AVTALT)
+    if (oppdatertVersjon.aktivitetType != AktivitetTypeData.MOTE && forrigeVersjon.tilDato != oppdatertVersjon.tilDato)
+        endringer.add(AVTALT_DATO_ENDRET)
+    if (erMøtetidspunktEndret(forrigeVersjon, oppdatertVersjon)) endringer.add(MOTE_TIDSPUNKT_ENDRET)
+    if (erMøtestedEndret(forrigeVersjon, oppdatertVersjon)) endringer.add(MOTE_STED_ENDRET)
+    if (erMøtekanalEndret(forrigeVersjon, oppdatertVersjon)) endringer.add(MOTE_KANAL_ENDRET)
+    if (erReferatOpprettet(forrigeVersjon, oppdatertVersjon)) endringer.add(REFERAT_OPPRETTET)
+    if (erReferatEndret(forrigeVersjon, oppdatertVersjon)) endringer.add(REFERAT_ENDRET)
+    if (erReferatPublisert(forrigeVersjon, oppdatertVersjon)) endringer.add(REFERAT_PUBLISERT)
+    if (erStillingsokEtikettEndret(forrigeVersjon, oppdatertVersjon)) endringer.add(STILLINGSOK_ETIKETT_ENDRET)
+    if (erForhaandsorienteringBlittLest(forrigeVersjon, oppdatertVersjon)) endringer.add(FORHAANDSORIENTERING_LEST)
+    if (harCvDeltBlittBesvart(forrigeVersjon, oppdatertVersjon)) endringer.add(DEL_CV_SVART)
+    if (erSøknadsstatusEndret(forrigeVersjon, oppdatertVersjon)) endringer.add(SOKNADSSTATUS_ENDRET)
+    if (erEndretTilIkkeFåttJobben(forrigeVersjon, oppdatertVersjon)) endringer.add(IKKE_FATT_JOBBEN)
+    if (erEndretTilFåttJobben(forrigeVersjon, oppdatertVersjon)) endringer.add(FATT_JOBBEN)
+    // TODO: Detaljer endret
+}
+
+private fun erMøtetidspunktEndret(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.aktivitetType != AktivitetTypeData.MOTE) return false
+    val startTidspunktEndret = forrigeVersjon.fraDato != oppdatertVersjon.fraDato
+    val sluttTidspunktEndret = forrigeVersjon.tilDato != forrigeVersjon.tilDato
+    return startTidspunktEndret || sluttTidspunktEndret
+}
+
+private fun erMøtestedEndret(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.aktivitetType != AktivitetTypeData.MOTE) return false
+    return forrigeVersjon.moteData.adresse != oppdatertVersjon.moteData.adresse
+}
+
+private fun erMøtekanalEndret(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.aktivitetType != AktivitetTypeData.MOTE) return false
+    return forrigeVersjon.moteData.kanal != oppdatertVersjon.moteData.kanal
+}
+
+private fun erReferatOpprettet(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.aktivitetType != AktivitetTypeData.MOTE || forrigeVersjon.aktivitetType != AktivitetTypeData.SAMTALEREFERAT) return false
+    return forrigeVersjon.moteData.referat.isNullOrEmpty() && !oppdatertVersjon.moteData.referat.isNullOrEmpty()
+}
+
+private fun erReferatEndret(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.aktivitetType != AktivitetTypeData.MOTE || forrigeVersjon.aktivitetType != AktivitetTypeData.SAMTALEREFERAT) return false
+    return !forrigeVersjon.moteData.referat.isNullOrEmpty() &&
+            (forrigeVersjon.moteData.referat != oppdatertVersjon.moteData.referat)
+}
+
+private fun erReferatPublisert(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.aktivitetType != AktivitetTypeData.MOTE || forrigeVersjon.aktivitetType != AktivitetTypeData.SAMTALEREFERAT) return false
+    return !forrigeVersjon.moteData.isReferatPublisert && oppdatertVersjon.moteData.isReferatPublisert
+}
+
+private fun erStillingsokEtikettEndret(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    // TODO: Hvilken type er dette aktuelt for?
+    return forrigeVersjon.stillingsSoekAktivitetData?.stillingsoekEtikett != oppdatertVersjon.stillingsSoekAktivitetData?.stillingsoekEtikett
+}
+
+private fun erForhaandsorienteringBlittLest(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.forhaandsorientering == null) return false
+    return forrigeVersjon.forhaandsorientering.lestDato == null && forrigeVersjon.forhaandsorientering.lestDato != null
+}
+
+private fun harCvDeltBlittBesvart(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.stillingFraNavData?.cvKanDelesData == null) return false
+    return forrigeVersjon.stillingFraNavData.cvKanDelesData.kanDeles == null && oppdatertVersjon.stillingFraNavData.cvKanDelesData.kanDeles != null
+}
+
+private fun erSøknadsstatusEndret(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.stillingFraNavData?.soknadsstatus == null) return false
+    return forrigeVersjon.stillingFraNavData.soknadsstatus != oppdatertVersjon.stillingFraNavData?.soknadsstatus
+}
+
+private fun erEndretTilIkkeFåttJobben(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.stillingFraNavData == null) return false
+    return forrigeVersjon.stillingFraNavData?.soknadsstatus != Soknadsstatus.IKKE_FATT_JOBBEN && oppdatertVersjon.stillingFraNavData?.soknadsstatus == Soknadsstatus.IKKE_FATT_JOBBEN
+}
+
+private fun erEndretTilFåttJobben(forrigeVersjon: AktivitetData, oppdatertVersjon: AktivitetData): Boolean {
+    if (forrigeVersjon.stillingFraNavData == null) return false
+    return forrigeVersjon.stillingFraNavData?.soknadsstatus != Soknadsstatus.FATT_JOBBEN && oppdatertVersjon.stillingFraNavData?.soknadsstatus == Soknadsstatus.FATT_JOBBEN
+}
+
+
+enum class AktivitetendringsType {
+    // Endringer som opptrer alene
+    BLE_HISTORISK, // OK
+    OPPRETTET, // OK
+    KASSERT,
+
+    // Endringer som kan opptre sammen med andre endringer
+    STATUS_ENDRET, // OK
+    AVTALT, // OK
+    AVTALT_DATO_ENDRET, // OK
+    STILLINGSOK_ETIKETT_ENDRET, // OK
+    MOTE_TIDSPUNKT_ENDRET, // OK
+    MOTE_STED_ENDRET, // OK
+    MOTE_KANAL_ENDRET, // OK
+    REFERAT_OPPRETTET, // OK
+    REFERAT_ENDRET, // OK
+    REFERAT_PUBLISERT, // OK
+    FORHAANDSORIENTERING_LEST, // OK
+    DEL_CV_SVART, // OK
+    SOKNADSSTATUS_ENDRET, // OK
+    IKKE_FATT_JOBBEN, // OK
+    FATT_JOBBEN, // OK
+    DETALJER_ENDRET,
 }
